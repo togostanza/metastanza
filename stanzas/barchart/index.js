3021ee9ac3df305f399c8847c5955b4e1779d086
Keita Urashima <ursm@ursm.jp>
2020-10-15 14:22:12 +0900

    Fetch data from barchart sparqlet

--- stanzas/barchart/index.js
+++ stanzas/barchart/index.js
@@ -1,8 +1,17 @@
+const url = 'http://togostanza.org/sparqlist/api/d3sparql_barchart';
+
 export default async function barchart(stanza, params) {
+  const data = await fetch(url).then(res => res.json());
+
+  const items = data.results.bindings.map((row) => {
+    return {
+      label: row.pref.value,
+      value: row.area.value
+    };
+  });
+
   stanza.render({
     template: 'stanza.html.hbs',
-    parameters: {
-      greeting: `Hello, ${params['say-to']}!`
-    }
+    parameters: {items}
   });
 }
