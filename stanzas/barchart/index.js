842905cc2ba5545fbf86141083aebdc5c5c5cfef
Keita Urashima <ursm@ursm.jp>
2020-10-15 15:58:28 +0900

    Improve label position

--- stanzas/barchart/index.js
+++ stanzas/barchart/index.js
@@ -42,7 +42,9 @@ function draw(el, dataset) {
     .call(d3.axisBottom(xScale))
     .selectAll("text")
     .style("text-anchor", "end")
-    .attr("transform", "rotate(-65)");
+    .attr("transform", "rotate(-90)")
+    .attr("dx", "-0.8em")
+    .attr("dy", "-0.6em");
 
   svg.append("g")
     .attr("transform", "translate(" + pad_left + "," + 0 + ")")
