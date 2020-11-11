34b714cae9775473972248a5fef47781adc67470
moriya-dbcls <moriya@enoki.local>
2020-11-11 18:08:10 +0900

    update manhattan plot

--- stanzas/manhattan_plot/index.js
+++ stanzas/manhattan_plot/index.js
@@ -11,15 +11,26 @@ export default async function manhattanPlot(stanza, params) {
 
   console.log(params.api);
   let dataset = await metastanza.getFormatedJson(params.api, stanza.root.querySelector('#chart'));
-  if (typeof dataset == "object") draw(dataset, stanza.root.querySelector('#chart'), parseFloat(params.p_thresh));
- 
+  if (typeof dataset == "object") draw(dataset, stanza, params);
 }
 
-async function draw(dataset, element, p_thresh) {
-  
+async function draw(dataset, stanza, params) {
   const width = 800;
   const height = 400;
-  
+  const marginLeft = 30;
+  const marginBottom = 30;
+  const areaWidth = width - marginLeft;
+  const areaHeight = height - marginBottom;
+
+  const chart_element = stanza.root.querySelector('#chart');
+  const control_element = stanza.root.querySelector('#control');
+
+  if (params.low_thresh == "") params.low_thresh = 0.5;
+  if (params.high_thresh == "") params.high_thresh = Infinity;
+  const low_thresh = parseFloat(params.low_thresh);
+  const high_thresh = parseFloat(params.high_thresh);
+  const even_and_odd = (params.even_and_odd == 'true');
+
   const chromosomes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "X", "Y"];
   const chromosomeNtLength = {
     hg38: {
@@ -49,47 +60,40 @@ async function draw(dataset, element, p_thresh) {
       Y: 57227415
     }
   }
-
-  const thresh =  Math.log10(p_thresh) * (-1);
   
-  let svg = d3.select(element).append("svg").attr("width", width).attr("height", height);
+  let svg = d3.select(chart_element).append("svg").attr("width", width).attr("height", height);
   let plot_g = svg.append("g").attr("id", "plot_group");
+  let axis_g = svg.append("g").attr("id", "axis");
+  let xlabel_g = svg.append("g").attr("id", "x_label");
+  let ylabel_g = svg.append("g").attr("id", "y_label");
   
-  let range = [];
+  let range = []; // [begin position, en _position]
   let max_log_p = 0;
   let max_log_p_int = 0;
-  
-  reRender();
-  
-  let xlabel_g = svg.append("g").attr("id", "x_label");
-  xlabel_g.selectAll(".xLabel")
-    .data(chromosomes)
-    .enter()
-    .append("text")
-    .attr("class", "xLabel")
-    .text(function(d){ return d; })
-    .attr("x", function(d){
-      let pos = chromosomeNtLength.hg38[d] / 2;
-      for(let ch of chromosomes){
-	if (ch == d) break;
-	pos += chromosomeNtLength.hg38[ch];
-      }
-      return pos / range[1] * width;
-    })
-    .attr("y", height - 10)
-    .attr("text-anchor", "middle");
-  
+  let total;
+
+  // axis line
+  axis_g.append("path")
+    .attr("d", "M " + marginLeft + ", " + areaHeight + " H " + width +  " Z")
+    .attr("class", "axis-line");
+  axis_g.append("path")
+    .attr("d", "M " + marginLeft + ", 0 V " + areaHeight +  " Z")
+    .attr("class", "axis-line");
+
+  // select range by drag
   let dragBegin = false;
   svg.on("mousedown", function(e, d){
-    dragBegin = d3.pointer(e)[0];
-    svg.append("rect")
-      .attr("fill", "rgba(128, 128, 128, 0.2)")
-      .attr("stroke", "black")
-      .attr("x", dragBegin)
-      .attr("y", 0)
-      .attr("width", 0)
-      .attr("height", height - 0.5 * height  / max_log_p_int)
-      .attr("id", "selector");
+    if (d3.pointer(e)[1] <= areaHeight) {
+      dragBegin = d3.pointer(e)[0];
+      svg.append("rect")
+	.attr("fill", "rgba(128, 128, 128, 0.2)")
+	.attr("stroke", "black")
+	.attr("x", dragBegin)
+	.attr("y", 0)
+	.attr("width", 0)
+	.attr("height", areaHeight)
+	.attr("id", "selector");
+    }
   })
     .on("mousemove", function(e, d){
       if(dragBegin){
@@ -107,9 +111,7 @@ async function draw(dataset, element, p_thresh) {
     .on("mouseup", function(e, d){
       if(dragBegin){
 	let dragEnd = d3.pointer(e)[0];
-	console.log(dragEnd);
-	console.log(dragBegin);
-	console.log(range[1] + " " + range[0]);
+	// re-render
 	if (-5 > dragEnd - dragBegin) {
 	  range = [dragEnd / width * (range[1] - range[0]) + range[0], dragBegin / width * (range[1] - range[0]) + range[0]];
 	  reRender();
@@ -121,22 +123,100 @@ async function draw(dataset, element, p_thresh) {
 	dragBegin = false;
       }
     });
+
+  // slider
+  let ctrl_svg = d3.select(control_element).append("svg").attr("width", width).attr("height", 20);
+  ctrl_svg.append("path")
+    .attr("d", "M " + marginLeft + ", 10 H " + width +  " Z")
+    .attr("stroke", "#888888")
+    .attr("stroke-width", "2px");
+  ctrl_svg.append("rect")
+    .attr("id", "slider")
+    .attr("x", marginLeft)
+    .attr("y", 2)
+    .attr("width", areaWidth)
+    .attr("height", 16)
+    .attr("fill", "#8888ff")
+    .call(
+      d3.drag()
+	.on("start", function(e){ dragBegin = e.x })
+	.on("drag", function(e){
+	  if(dragBegin) {
+	    let slider = ctrl_svg.select("rect#slider");
+	    let delta = e.x - dragBegin;
+	    if (parseFloat(slider.attr("x")) + delta < marginLeft) delta = (parseFloat(slider.attr("x")) - marginLeft) * (-1);
+	    else if (parseFloat(slider.attr("x")) + parseFloat(slider.attr("width")) + delta > width) delta = width - (parseFloat(slider.attr("x")) + parseFloat(slider.attr("width")));
+	    slider.attr("transform", "translate(" + delta + ", 0)");
+	  }
+	})
+	.on("end", function(e){
+	  if(dragBegin){
+	    // re-render
+	    let slider = ctrl_svg.select("rect#slider");
+	    let delta = e.x - dragBegin;
+	    if (parseFloat(slider.attr("x")) + delta < marginLeft) delta = (parseFloat(slider.attr("x")) - marginLeft) * (-1);
+	    else if (parseFloat(slider.attr("x")) + parseFloat(slider.attr("width")) + delta > width) delta = width - (parseFloat(slider.attr("x")) + parseFloat(slider.attr("width")));
+	    let move = delta / areaWidth * total;
+	    range = [range[0] + move, range[1] + move];
+	    reRender();
+	    dragBegin = false;
+	  }
+	})
+    );
+  
+  // button
+  let ctrl_button = d3.select(control_element).append("div").attr("id", "ctrl_button");
+  ctrl_button.append("input").attr("type", "button").attr("value", "-")
+    .on("click", function(){
+      let begin = range[0] - (range[1] - range[0]) / 2;
+      let end = range[1] + (range[1] - range[0]) / 2;
+      if (begin < 0) {
+	begin = 0;
+	end = (range[1] - range[0]) * 2;
+	if (end > total) end = total;
+      }else if (end > total) {
+	end = total;
+	begin = total - (range[1] - range[0]) * 2;
+	if (begin < 0) begin - 0;
+      }
+      range = [begin, end];
+      reRender();
+    });
+  ctrl_button.append("input").attr("type", "button").attr("value","+")
+    .on("click", function(){
+      let begin = range[0] + (range[1] - range[0]) / 4;
+      let end = range[1] - (range[1] - range[0]) / 4;
+      range = [begin, end];
+      reRender();
+    });
+  ctrl_button.append("input").attr("type", "button").attr("value","reset")
+    .on("click", function(){
+      range = [];
+      reRender();
+    });
+
+  reRender();
   
   function reRender(){
     
-    if (range[0] == undefined) range = [0,  Object.values(chromosomeNtLength.hg38).reduce((sum, value) => sum +value)];
-
-    console.log(range[0]);
-    console.log(range[1]);
+    if (range[0] == undefined) {
+      range = [0,  Object.values(chromosomeNtLength.hg38).reduce((sum, value) => sum +value)];
+      total = range[1];
+    }
     
     max_log_p = 0;
     
     plot_g.html("");
+    xlabel_g.html("");
+    ylabel_g.html("");
+    
     plot_g.selectAll(".plot")
       .data(dataset)
       .enter()
+    // filter: display range
       .filter(function(d){
 	if (!d.pos) {
+	  // calculate  accumulated position
 	  let pos = 0;
 	  for(let ch of chromosomes){
 	    if (ch == d.Chromosome) break;
@@ -146,18 +226,27 @@ async function draw(dataset, element, p_thresh) {
 	}
 	return range[0] <= d.pos && d.pos <= range[1];
       })
-      .filter(function(d){ return Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) > 0.5 } )
+    // filter: low p-value
+      .filter(function(d){ return Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) > low_thresh })
       .append("circle")
-      .attr("class", function(d){ return "plot ch_" + d.Chromosome })
+      .attr("class", function(d, i){
+	if (even_and_odd) {
+	  let tmp = "even";
+	  if (d.Chromosome == "X" || parseInt(d.Chromosome) % 2 == 1) tmp = "odd";
+	  return "plot ch_" + tmp;
+	}
+	return "plot ch_" + d.Chromosome
+      })
       .attr("cx", function(d){
-	return (d.pos - range[0]) / (range[1] - range[0]) * width })
+	return (d.pos - range[0]) / (range[1] - range[0]) * areaWidth + marginLeft})
       .attr("cy",function(d){
-	//set max
+	// set max log(p-value)
 	if (max_log_p < Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1)) max_log_p = Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1);
-	return height;
+	return areaHeight;
       })
       .attr("r", 2)
-      .filter(function(d){return Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) > thresh })
+    // filter: high p-value
+      .filter(function(d){return Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) > high_thresh })
       .classed("over-thresh-plot", true)
       .on("mouseover", function(e, d){
 	svg.append("text").text(d.dbSNP_RS_ID + ", " + d.Symbol)
@@ -166,11 +255,56 @@ async function draw(dataset, element, p_thresh) {
       .on("mouseout", function(e, d){
 	svg.select("#popup_text").remove();
       });
-    
-    max_log_p_int = Math.floor(max_log_p) + 1;
-    
-    plot_g.selectAll(".plot")
-      .attr("cy", function(d){ return height - Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) * height  / max_log_p_int});
 
+    // set 'cy' from max log(p-value) (int)
+    max_log_p_int = Math.floor(max_log_p);
+    plot_g.selectAll(".plot")
+      .attr("cy", function(d){ return areaHeight  - (Math.log10(parseFloat(d.CLR_C_BMI_pv)) * (-1) - low_thresh) * areaHeight  / max_log_p_int});
+
+    // x axis label
+    xlabel_g.selectAll(".xLabel")
+      .data(chromosomes)
+      .enter()
+      .append("text")
+      .attr("class", "axisLabel xLabel")
+      .text(function(d){ return d; })
+      .attr("x", function(d){
+	let pos = chromosomeNtLength.hg38[d] / 2;
+	for(let ch of chromosomes){
+	  if (ch == d) break;
+	  pos += chromosomeNtLength.hg38[ch];
+	}
+	return (pos - range[0]) / (range[1] - range[0]) * areaWidth + marginLeft;
+      })
+      .attr("y", areaHeight + 20);
+
+    // y axis label
+    for (let i = Math.floor(low_thresh) + 1; i <=  max_log_p_int; i++) {
+      let y = areaHeight  - (i - low_thresh) * areaHeight / max_log_p_int;
+      ylabel_g.append("text").text(i)
+	.attr("class", "axisLabel yLabel")
+	.attr("x", marginLeft - 16)
+	.attr("y", y)
+	.attr("text-anchor", "end");
+      ylabel_g.append("path")
+	.attr("class", "axis-line")
+	.attr("d", "M " + (marginLeft - 10) + ", " + y + " H " + marginLeft + " Z");
+    }
+    //// y zero (low_thresh)
+    ylabel_g.append("text").text(low_thresh)
+      .attr("class", "axisLabel yLabel")
+      .attr("x", marginLeft - 16)
+      .attr("y", areaHeight)
+      .attr("text-anchor", "end");
+    ylabel_g.append("path")
+      .attr("class", "axis-line")
+      .attr("d", "M " + (marginLeft - 10) + ", " + areaHeight + " H " + marginLeft + " Z");
+
+    // slider
+    ctrl_svg.select("rect#slider")
+      .attr("x", marginLeft + range[0] / total * areaWidth)
+      .attr("width", (range[1] - range[0]) / total * areaWidth)
+      .attr("transform", "translate(0, 0)");
+	
   }
 }
