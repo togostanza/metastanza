fc6e8bdd29b9fe16e9951e49aea5729b1ffbbad0
Keita Urashima <ursm@ursm.jp>
2020-11-11 16:27:13 +0900

    Vega-based barchart stanza

--- stanzas/barchart/index.js
+++ stanzas/barchart/index.js
@@ -1,81 +1,56 @@
-import * as d3 from 'd3';
-import metastanza from '@/lib/metastanza_utils.js';
+import * as Vega from 'vega';
+import * as VegaLite from 'vega-lite';
+import { Handler as VegaTooltipHandler } from 'vega-tooltip';
 
 export default async function barchart(stanza, params) {
-  
-  stanza.render({
-    template: 'stanza.html.hbs',
-    parameters: {
-      title: params.title
+  const dataset = await fetch(params['src-url']).then(res => res.json());
+
+  const values = dataset.data.flatMap((item) => {
+    return dataset.series.map((itemLabel) => {
+      return {
+        value: item[itemLabel],
+        barLabel: item.label,
+        itemLabel,
+      };
+    });
+  });
+
+  const {spec} = VegaLite.compile({
+    data: {
+      values
+    },
+    title: params['chart-title'],
+    mark: 'bar',
+    width: {
+      step: 64
+    },
+    encoding: {
+      x: {
+        field: 'barLabel',
+        title: null
+      },
+      y: {
+        aggregate: 'sum',
+        field: 'value',
+        title: null
+      },
+      color: {
+        field:  'itemLabel',
+        title:  null,
+        legend: params['show-legend'] === 'show-legend' ? {} : null
+      },
+      tooltip: {
+        field: 'itemLabel'
+      }
     }
   });
-  
-  const element = stanza.root.querySelector('#chart');
-  //const dataset = await metastanza.getJsonFromSparql(params.api, element, params.post_params, params.label_var_name, params.value_var_name);
-  const dataset = await metastanza.getFormatedJson(params.api, element, params.post_params);
-  if (typeof(dataset) === "object") draw(stanza.root.querySelector('#chart'), dataset);
-}
 
+  const view = new Vega.View(Vega.parse(spec), {
+    renderer: 'svg',
+    container: stanza.root.querySelector('main')
+  });
 
-function draw(el, dataset) {
-  let width = 800;
-  let height = 400;
-  let pad_left = 50;
-  let pad_right = 10;
-  let pad_top = 10;
-  let pad_bottom = 130;
+  view.tooltip(new VegaTooltipHandler().call);
 
-  // SVG
-  const svg = d3.select(el).append("svg").attr("width", width).attr("height", height);
-
-  // axis scale
-  let xScale = d3.scaleBand()
-      .rangeRound([pad_left, width - pad_right])
-      .padding(0.1)
-      .domain(dataset.data.map(function(d) { return d.label; }));
-  
-  let yScale = d3.scaleLinear()
-      .domain( [0, d3.max(dataset.data, function(d) {
-	let sum  = 0;
-	Object.keys(d).forEach(key => { if(key != "label") sum += d[key]; })
-	return sum;
-      }) ])
-      .range([height - pad_bottom, pad_top]);
-  
-  // render axis
-  svg.append("g")
-    .attr("transform", "translate(" + 0 + "," + (height - pad_bottom) + ")")
-    .call(d3.axisBottom(xScale))
-    .selectAll("text")
-    .style("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("dx", "-0.8em")
-    .attr("dy", "-0.6em");
-  
-  svg.append("g")
-    .attr("transform", "translate(" + pad_left + "," + 0 + ")")
-    .call(d3.axisLeft(yScale));
-  
-  let stack = d3.stack().keys(dataset.series).order(d3.stackOrderDescending);
-  let series = stack(dataset.data);
-
-  console.log(series);
-  
-  // render bar
-  let data_bar_g = svg.append("g");
-  let series_g = data_bar_g.selectAll(".series")
-      .data(series)
-      .enter()
-      .append("g")
-      .attr("class", function(d, i){ return "series series_" + i; });
-  series_g.selectAll(".bar")
-    .data(function(d) { return d; })
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return xScale(d.data.label); })
-    .attr("y", function(d) { return yScale(d[1]); })
-    .attr("width", xScale.bandwidth())
-    .attr("height", function(d) { return yScale(d[0]) - yScale(d[1]); }); 
-
+  await view.runAsync();
 }
