d64b6b48a9bd420bba4b1857d2ff44ccd863b683
reika0717 <reikahirahara@gmail.com>
2021-01-21 12:04:47 +0900

    implement sort function

--- stanzas/table_with_pagination/app.vue
+++ stanzas/table_with_pagination/app.vue
@@ -1,16 +1,20 @@
 <template>
   <table>
     <thead>
-      <tr v-if="table_data.head">
-        <th v-for="(label, index) in filtered_head_labels" :key="index">
-          {{ label }}
+      <tr v-if="adjustedTableData.head">
+        <th v-for="(head, index) in filteredHead" :key="index">
+          {{ head.label }}
+          <span
+            :class="['icon', 'sortIcon', sortState.active === head.id ? sortState.order : '']"
+            @click="SortData(head.id)"
+            ></span>
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(col, index) in table_data.body" :key="index">
-        <td v-for="(datam, index_2) in filtered_head_vars" :key="index_2">
-          {{ col[datam].value }}
+      <tr v-for="(col, index) in adjustedTableData.body" :key="index">
+        <td v-for="(head, index2) in filteredHead" :key="index2">
+          {{ col[head.id].value }}
         </td>
       </tr>
     </tbody>
@@ -25,34 +29,66 @@ export default defineComponent({
   props: metadata["stanza:parameter"].map(p => p["stanza:key"]),
   setup(params) {
     //data
-    const table_data = ref([])
-    const filtered_head_labels = ref([])
-    const filtered_head_vars = ref([])
+    const tableData = ref([])
+    const adjustedTableData = ref([])
+    const filteredHead = ref([])
+    const sortState = reactive({
+      active: '',
+      order: 'desc',
+    })
 
     //methods
-    const getData = () => {
+    const GetData = () => {
       fetch(params.table_data_api)
         .then(response => response.json())
         .then(data => {
-          table_data.value = data
+          console.log(data)
+          tableData.value = data
+          adjustedTableData.value = data
           data.head.labels.forEach((label,index) => {
             if(label !== null) {
-              filtered_head_labels.value.push(label)
-              filtered_head_vars.value.push(data.head.vars[index])
+              filteredHead.value.push({
+                id: data.head.vars[index],
+                label: label
+              })
             }
           })
         });
     };
 
+    const SortData = (id) => {
+      sortState.active = id
+      sortState.order = sortState.order === 'asc' ? 'desc' : 'asc'
+      switch (sortState.order) {
+        case "desc":
+          tableData.value.body.sort((a, b) => {
+            return a[sortState.active].value.toLowerCase() <
+              b[sortState.active].value.toLowerCase()
+              ? -1
+              : 1;
+          });
+          break;
+        case "asc":
+          tableData.value.body.sort((b, a) => {
+            return a[sortState.active].value.toLowerCase() <
+              b[sortState.active].value.toLowerCase()
+              ? -1
+              : 1;
+          });
+          break;
+      }
+    }
+
     // mounted
     onMounted(() => {
-      getData();
+      GetData();
     });
 
     return {
-      table_data,
-      filtered_head_labels,
-      filtered_head_vars,
+      adjustedTableData,
+      filteredHead,
+      sortState,
+      SortData,
       params
     }
   }
