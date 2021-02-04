51e38c5fcdaeb3f804807bac0dafa41615dad940
c-nakashima <nakashima@penqe.com>
2021-02-04 18:37:48 +0900

    add text stanza data

--- stanzas/text/index.js
+++ stanzas/text/index.js
@@ -1,8 +1,35 @@
+import { getFormatedJson, appendDlButton } from "@/lib/metastanza_utils.js";
+
 export default async function text(stanza, params) {
+  const dataset = await getFormatedJson(
+    params.api,
+    // stanza.root.querySelector("#chart")
+  );
+  console.log(dataset);
+  console.log(dataset.value);
+  
   stanza.render({
     template: "stanza.html.hbs",
     parameters: {
-      greeting: `Hello, ${params["say-to"]}!`,
-    },
+      fields: [
+        {
+          label: 'First name',
+          required: true
+        },
+        {
+          label: 'Middle name',
+          required: false
+        },
+        {
+          label: 'Last name',
+          required: true
+        },
+      ],
+      rows: [
+        {
+          value: dataset.value,
+        }
+      ]
+    }
   });
 }
