df11261351df1ffe2b017c02cfaac072e0cf7508
c-nakashima <nakashima@penqe.com>
2021-02-04 20:14:12 +0900

    add data to scorecard

--- stanzas/scorecard/index.js
+++ stanzas/scorecard/index.js
@@ -1,8 +1,21 @@
+import { getFormatedJson, appendDlButton } from "@/lib/metastanza_utils.js";
+import { keys } from "vega-lite";
+
 export default async function scorecard(stanza, params) {
+  const dataset = await getFormatedJson(
+    params.api,
+  );
+  console.log(dataset);
+  console.log(Object.values(dataset)[0]);
   stanza.render({
     template: "stanza.html.hbs",
     parameters: {
-      greeting: `Hello, ${params["say-to"]}!`,
-    },
+      scorecards: [
+        {
+          key: Object.keys(dataset)[0],
+          value: Object.values(dataset)[0],
+        }
+      ]
+    }
   });
 }
--- stanzas/text/index.js
+++ stanzas/text/index.js
@@ -5,26 +5,9 @@ export default async function text(stanza, params) {
     params.api,
     // stanza.root.querySelector("#chart")
   );
-  console.log(dataset);
-  console.log(dataset.value);
-  
   stanza.render({
     template: "stanza.html.hbs",
     parameters: {
-      fields: [
-        {
-          label: 'First name',
-          required: true
-        },
-        {
-          label: 'Middle name',
-          required: false
-        },
-        {
-          label: 'Last name',
-          required: true
-        },
-      ],
       rows: [
         {
           value: dataset.value,
