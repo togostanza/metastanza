835fe15a83d575bac1ab32b94ac835492eb463ad
c-nakashima <nakashima@penqe.com>
2021-03-18 13:56:04 +0900

    add text download to text stanza

--- stanzas/text/index.js
+++ stanzas/text/index.js
@@ -2,6 +2,15 @@ import loadData from "@/lib/load-data";
 
 export default async function text(stanza, params) {
   const dataset = await loadData(params["data-url"], params["data-type"]);
+
+  console.log(dataset.value)
+  const textBlob = new Blob([dataset.value], {
+    type: "text/plain",
+  })
+
+  const textUrl = URL.createObjectURL(textBlob);
+  console.log(textUrl)
+
   stanza.render({
     template: "stanza.html.hbs",
     parameters: {
@@ -10,6 +19,7 @@ export default async function text(stanza, params) {
           value: dataset.value,
         },
       ],
+      textUrl: URL.createObjectURL(textBlob)
     },
   });
 }
