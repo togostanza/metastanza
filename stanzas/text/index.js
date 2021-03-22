bfdf94a74dd63a55f785216d452e362a3e99f5ad
c-nakashima <nakashima@penqe.com>
2021-03-22 14:26:22 +0900

    organize text parameter

--- stanzas/text/index.js
+++ stanzas/text/index.js
@@ -1,9 +1,8 @@
 import loadData from "@/lib/load-data";
 
 export default async function text(stanza, params) {
+  stanza.importWebFontCSS('https://use.fontawesome.com/releases/v5.6.3/css/all.css');
   const dataset = await loadData(params["data-url"], params["data-type"]);
-
-  console.log(dataset.value);
   const textBlob = new Blob([dataset.value], {
     type: "text/plain",
   });
@@ -22,4 +21,30 @@ export default async function text(stanza, params) {
       textUrl: URL.createObjectURL(textBlob),
     },
   });
+  const width = params["width"];
+  const height = params["height"];
+  const padding = params["padding"];
+  const table = stanza.root.querySelector("table");
+  table.setAttribute(
+    `style`,
+    `width: ${width}px; height: ${height}px; padding: ${padding}px;`
+  );
+
+  const menu = stanza.root.querySelector(".menu");
+  switch (params["metastanza-menu-placement"]) {
+    case "top-left":
+      break;
+    case "top-right":
+      menu.setAttribute("style", "justify-content: flex-end;");
+      break;
+    case "bottom-left":
+      menu.setAttribute("style", "flex-direction: column-reverse;");
+      break;
+    case "bottom-right":
+      menu.setAttribute("style", "justify-content flex-end; flex-direction: column-reverse;");
+      break;
+    case "none":
+      menu.setAttribute("style", "display: none;");
+      break;
+  }
 }
