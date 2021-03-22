b2b2463443b4e42b6f60f3a1bed9ce521d2864ec
c-nakashima <nakashima@penqe.com>
2021-03-22 21:46:56 +0900

    organize parameters of table-with-scroll

--- stanzas/table-with-scroll/index.js
+++ stanzas/table-with-scroll/index.js
@@ -3,5 +3,11 @@ import App from "./app.vue";
 
 export default async function tableWithPagination(stanza, params) {
   const main = stanza.root.querySelector("main");
+  main.setAttribute(
+    `style`,
+    `width: ${params["width"]}px;
+    height: ${params["height"]}px;
+    padding: ${params["padding"]}px;`
+  );
   createApp(App, params).mount(main);
 }
