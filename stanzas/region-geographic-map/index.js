2bb3752241337a946510e842877aeb6c52ffec17
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2021-04-27 11:24:40 +0900

    add set group amount option to region geo map

--- stanzas/region-geographic-map/index.js
+++ stanzas/region-geographic-map/index.js
@@ -1,58 +1,66 @@
 import vegaEmbed, { vega } from "vega-embed";
 
 export default async function regionGeographicMap(stanza, params) {
-
   function css(key) {
     return getComputedStyle(stanza.root.host).getPropertyValue(key);
   }
 
-  const vegaJson = await fetch("https://vega.github.io/vega/examples/county-unemployment.vg.json").then((res) => res.json());
-
-
-  const 
-    width = params["width"], //metadata.jsにパラメータを定義
-    height = params["height"];
+  const vegaJson = await fetch(
+    "https://vega.github.io/vega/examples/county-unemployment.vg.json"
+  ).then((res) => res.json());
 
   const data = [
     {
       name: "unemp",
       url: "https://vega.github.io/vega/data/unemployment.tsv",
-      format: {"type": "tsv", "parse": "auto"}
+      format: { type: "tsv", parse: "auto" },
     },
     {
       name: "counties",
       url: "https://vega.github.io/vega/data/us-10m.json",
-      format: {type: "topojson", "feature": "counties"},
+      format: { type: "topojson", feature: "counties" },
       transform: [
-        { type: "lookup", from: "unemp", key: "id", fields: ["id"], values: ["rate"] },
-        { type: "filter", expr: "datum.rate != null" }
-      ]
-    }
-  ]
+        {
+          type: "lookup",
+          from: "unemp",
+          key: "id",
+          fields: ["id"],
+          values: ["rate"],
+        },
+        { type: "filter", expr: "datum.rate != null" },
+      ],
+    },
+  ];
 
   const projections = [
     {
       name: "projection",
-      type: "albersUsa"
-    }
-  ]
+      type: "albersUsa",
+    },
+  ];
 
-  const scales =  [
+  const colorRangeMax = [
+    "var(--togostanza-series-0-color)",
+    "var(--togostanza-series-1-color)",
+    "var(--togostanza-series-2-color)",
+    "var(--togostanza-series-3-color)",
+    "var(--togostanza-series-4-color)",
+    "var(--togostanza-series-5-color)",
+    "var(--togostanza-series-6-color)",
+    "var(--togostanza-series-7-color)",
+  ];
+
+  const colorRange = 
+    colorRangeMax.slice(0, Number(params["group-amount"]));
+
+  const scales = [
     {
       name: "userColor",
       type: "quantize",
       domain: [0, 0.15],
-      range: [
-        "var(--togostanza-series-0-color)" ,
-        "var(--togostanza-series-1-color)" ,
-        "var(--togostanza-series-2-color)" ,
-        "var(--togostanza-series-3-color)" ,
-        "var(--togostanza-series-4-color)" ,
-        "var(--togostanza-series-5-color)" ,
-        "var(--togostanza-series-6-color)"
-      ]
-    }
-  ]
+      range: colorRange
+    },
+  ];
 
   const legends = [
     {
@@ -63,40 +71,40 @@ export default async function regionGeographicMap(stanza, params) {
       titleFont: "var(--togostanza-font-family)",
       titleFontWeight: "var(--togostanza-title-font-weight)",
       format: "0.1%",
-    }
-  ]
+    },
+  ];
 
   const marks = [
     {
       type: "shape",
-      from: {data: "counties"},
+      from: { data: "counties" },
       encode: {
-        enter: { tooltip: {signal: "format(datum.rate, '0.1%')"}},
-        hover: { 
-          fill: { value: "var(--togostanza-hover-color)"} 
+        enter: { tooltip: { signal: "format(datum.rate, '0.1%')" } },
+        hover: {
+          fill: { value: "var(--togostanza-hover-color)" },
         },
-        update: { 
-          fill: { scale: "userColor", field: "rate"},
-          stroke: params["border"] ? {"value": "var(--togostanza-region-border-color)"} : ""
+        update: {
+          fill: { scale: "userColor", field: "rate" },
+          stroke: params["border"]
+            ? { value: "var(--togostanza-region-border-color)" }
+            : "",
         },
       },
-      transform: [
-        { type: "geoshape", projection: "projection" },
-      ]
-    }
-  ]
+      transform: [{ type: "geoshape", projection: "projection" }],
+    },
+  ];
 
   const spec = {
     $schema: "https://vega.github.io/schema/vega/v5.json",
-    width,
-    height,
+    width: 1000,
+    height: 500,
     signals: vegaJson.signals,
     data,
     projections,
     scales,
     legends: params["legend"] ? legends : [],
     marks,
-  }
+  };
 
   const el = stanza.root.querySelector("main");
   const opts = {
