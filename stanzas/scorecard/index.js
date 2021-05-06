65a5d6d6eee2a588e4c9b6324996d6129139b2be
Chisato Nakashima <chisatonakashima@Chisatos-Mac-Book-Pro.local>
2021-05-06 10:36:21 +0900

    adjust padding

--- stanzas/scorecard/index.js
+++ stanzas/scorecard/index.js
@@ -29,29 +29,32 @@ export default async function scorecard(stanza, params) {
   const main = stanza.root.querySelector("main");
   main.parentNode.setAttribute(
     `style`,
-    `width: ${width}px; height: ${height}px; padding: ${padding}px; background-color: var(--togostanza-background-color);`
+    `background-color: var(--togostanza-background-color);`
   );
 
   const chartWrapper = stanza.root.querySelector(".chart-wrapper");
-  chartWrapper.setAttribute(`style`, `width: ${width}px; height: ${height}px;`);
+  chartWrapper.setAttribute(`style`, `width: ${width}px; height: ${height}px; padding: ${padding}px`);
 
-  const key = stanza.root.querySelector("#scorecardKey");
-  const value = stanza.root.querySelector("#scorecardValue");
+  const scorecardSvg = stanza.root.querySelector("#scorecardSvg");
+  scorecardSvg.setAttribute('height',`${Number(css("--togostanza-key-font-size")) + Number(css("--togostanza-value-font-size"))}`)
+
+  const key = stanza.root.querySelector("#key");
+  const value = stanza.root.querySelector("#value");
   if (params["legend"] === "false") {
     key.setAttribute(`style`, `display: none;`);
   }
 
-  key.setAttribute("x", `${width / 2}px`);
   key.setAttribute("y", Number(css("--togostanza-key-font-size")));
-  value.setAttribute("x", `${width / 2}px`);
+  key.setAttribute("fill",css("--togostanza-key-font-color"));
   value.setAttribute(
     "y",
     Number(css("--togostanza-key-font-size")) +
       Number(css("--togostanza-value-font-size"))
   );
+  value.setAttribute("fill",css("--togostanza-value-font-color"));
   key.setAttribute("font-size", css("--togostanza-key-font-size"));
   value.setAttribute("font-size", css("--togostanza-value-font-size"));
-
+  
   //menu button placement
   appendDlButton(
     stanza.root.querySelector(".chart-wrapper"),
@@ -62,6 +65,8 @@ export default async function scorecard(stanza, params) {
 
   const menuButton = stanza.root.querySelector("#dl_button");
   const menuList = stanza.root.querySelector("#dl_list");
+  console.log('dl_button',menuButton);
+  console.log('menuList',menuList)
   switch (params["metastanza-menu-placement"]) {
     case "top-left":
       menuButton.setAttribute("class", "dl-top-left");
