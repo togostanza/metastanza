c4312f1e004e8ff2ba5129f6007faac9cdba1682
Yoji Shidara <dara@shidara.net>
2021-06-22 10:28:55 +0900

    Migrate PaginationTable to class-based

--- stanzas/pagination-table/index.js
+++ stanzas/pagination-table/index.js
@@ -1,9 +1,14 @@
+import Stanza from "togostanza/stanza";
+
 import { createApp } from "vue";
 import App from "./app.vue";
 
-export default async function paginationTable(stanza, params) {
-  const main = stanza.root.querySelector("main");
-  main.parentNode.style.backgroundColor = "var(--togostanza-background-color)";
-  main.parentNode.style.padding = params["padding"];
-  createApp(App, params).mount(main);
+export default class PaginationTable extends Stanza {
+  async render() {
+    const main = this.root.querySelector("main");
+    main.parentNode.style.backgroundColor =
+      "var(--togostanza-background-color)";
+    main.parentNode.style.padding = this.params["padding"];
+    createApp(App, this.params).mount(main);
+  }
 }
