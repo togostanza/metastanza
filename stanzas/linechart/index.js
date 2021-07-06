e4e2c4b714654a71bebe3e951bd9913b11a19f92
tmaeda <tmaeda@ruby-sapporo.org>
2021-07-06 11:58:13 +0900

    Use menu in linechart

--- stanzas/linechart/index.js
+++ stanzas/linechart/index.js
@@ -2,9 +2,19 @@ import Stanza from "togostanza/stanza";
 
 import vegaEmbed from "vega-embed";
 import loadData from "@/lib/load-data";
-import { appendDlButton } from "@/lib/metastanza_utils.js";
+import {
+  downloadSvgMenuItem,
+  downloadPngMenuItem,
+} from "@/lib/metastanza_utils.js";
 
 export default class Linechart extends Stanza {
+  menu() {
+    return [
+      downloadSvgMenuItem(this, "linechart"),
+      downloadPngMenuItem(this, "linechart"),
+    ];
+  }
+
   async render() {
     const css = (key) => getComputedStyle(this.element).getPropertyValue(key);
 
@@ -234,38 +244,5 @@ export default class Linechart extends Stanza {
       renderer: "svg",
     };
     await vegaEmbed(el, spec, opts);
-
-    //menu button placement
-    appendDlButton(
-      this.root.querySelector(".chart-wrapper"),
-      this.root.querySelector("svg"),
-      "linechart",
-      this.root
-    );
-
-    const menuButton = this.root.querySelector("#dl_button");
-    const menuList = this.root.querySelector("#dl_list");
-    switch (this.params["metastanza-menu-placement"]) {
-      case "top-left":
-        menuButton.setAttribute("class", "dl-top-left");
-        menuList.setAttribute("class", "dl-top-left");
-        break;
-      case "top-right":
-        menuButton.setAttribute("class", "dl-top-right");
-        menuList.setAttribute("class", "dl-top-right");
-        break;
-      case "bottom-left":
-        menuButton.setAttribute("class", "dl-bottom-left");
-        menuList.setAttribute("class", "dl-bottom-left");
-        break;
-      case "bottom-right":
-        menuButton.setAttribute("class", "dl-bottom-right");
-        menuList.setAttribute("class", "dl-bottom-right");
-        break;
-      case "none":
-        menuButton.setAttribute("class", "dl-none");
-        menuList.setAttribute("class", "dl-none");
-        break;
-    }
   }
 }
