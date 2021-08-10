6ca861926e9c2491a419e78a1132ab733f1263fc
c-nakashima <nakashima@penqe.com>
2021-08-10 11:01:23 +0900

    revise data temporarily

--- stanzas/venn-stanza/index.js
+++ stanzas/venn-stanza/index.js
@@ -20,17 +20,27 @@ export default class VennStanza extends Stanza {
   async render() {
     appendCustomCss(this, this.params["custom-css-url"]);
     const css = (key) => getComputedStyle(this.element).getPropertyValue(key);
-    // // venn.js usable data
-    // var sets = [
-    //   { sets: ['サイトA'], size: 12 },
-    //   { sets: ['サイトB'], size: 12 },
-    //   { sets: ['サイトC'], size: 11 },
-    //   { sets: ['サイトD'], size: 10 },
-    //   { sets: ['サイトA', 'サイトB'], size: 2 },
-    //   { sets: ['サイトB', 'サイトC'], size: 2 },
-    //   { sets: ['サイトA', 'サイトC'], size: 2 },
-    //   { sets: ['サイトB', 'サイトD'], size: 2 },
+    // // TogoGenome's data structure exclusive
+    // let sets = [
+    //   { sets: ['A'], size: 52 },
+    //   { sets: ['B'], size: 73 },
+    //   { sets: ['C'], size: 183 },
+    //   { sets: ['A', 'B'], size: 9 },
+    //   { sets: ['B', 'C'], size: 20 },
+    //   { sets: ['A', 'C'], size: 591 },
+    //   { sets: ['A','B', 'C'], size: 5568 },
     // ];
+
+    // Venn.js usable data
+    let sets = [
+      { sets: ['A'], size: 6241 },
+      { sets: ['B'], size: 5670 },
+      { sets: ['C'], size: 6362 },
+      { sets: ['A', 'B'], size: 9 + 5568 },
+      { sets: ['B', 'C'], size: 20 + 5568 },
+      { sets: ['A', 'C'], size: 591 + 5568 },
+      { sets: ['A', 'B', 'C'], size: 5568 },
+    ];
 
     //get data
     let dataset = await loadData(
@@ -45,7 +55,7 @@ export default class VennStanza extends Stanza {
       json[i].sets = json[i].sets.split(', ');
       json[i].size = Number(json[i].size);
     }
-    const sets = json;
+    // const sets = json;
 
     this.renderTemplate(
       {
@@ -86,7 +96,7 @@ export default class VennStanza extends Stanza {
       .data(sets)
       .style('fill-opacity', css('--togostanza-opacity'))
       .style('fill', function (d, i) {
-        console.log('d',d);
+        console.log('d', d);
         return colorScheme[i];
       })
       .style('stroke-width', css('--togostanza-border-width'))
@@ -96,113 +106,82 @@ export default class VennStanza extends Stanza {
       });
 
     //text
-    originalSvg
-      .selectAll('#venn .venn-circle text')
+    // originalSvg
+    //   .selectAll('#venn .venn-circle text')
+    //   .style('fill', function (d, i) {
+    //     return css('--togostanza-label-font-color');
+    //   })
+    //   .style('font-family', css('--togostanza-font-family'))
+    //   .style('font-size', css('--togostanza-label-font-size') + 'px')
+    //   .style('font-weight', '100');
+
+
+    const interactiveChart = venn.VennDiagram();
+    const interactiveElement = this.root.querySelector('#interactive');
+    const interactiveSvg = d3.select(interactiveElement);
+
+    // draw interactive venn diagram svg
+    interactiveSvg
+      .datum(sets)
+      .call(interactiveChart);
+
+    //tooltip
+    const main = this.root.querySelector('main');
+    const tooltip =
+      d3.select(main)
+        .append('div')
+        .attr('class', 'venntooltip');
+    //path
+    interactiveSvg
+      .selectAll('path')
       .style('fill', function (d, i) {
-        return css('--togostanza-label-font-color');
-      })
+        return colorScheme[i];
+      })     
+      .style('stroke-opacity', 0)
+      .style('stroke', '#333')
+      .style('stroke-width', 3);
+
+    //text
+    interactiveSvg
+      .selectAll('#venn .venn-circle text')
+      // .style('fill', function (d, i) {
+      //   return css('--togostanza-label-font-color');
+      // })
+      .style('fill', function (d, i) {
+        return colorScheme[i];
+      })   
       .style('font-family', css('--togostanza-font-family'))
       .style('font-size', css('--togostanza-label-font-size') + 'px')
       .style('font-weight', '100');
 
-
-    // // add a tooltip
-    // const body = this.root.querySelector('body');
-    // let tooltip = d3.select(body).append("div")
-    //   .attr("class", "venntooltip");
-
-    // // add listeners to all the groups to display tooltip on mouseover
-    // originalSvg.selectAll("g")
-    //   .data(sets)
-    //   .on("mouseover", function (d, i) {
-    //     console.log('d',d);
-    //     console.log('sets',sets)
-    //     // sort all the areas relative to the current item
-    //     // venn.sortAreas(originalSvg, d);
-    //     // Display a tooltip with the current size
-    //     tooltip.transition().duration(400).style("opacity", .9);
-    //     tooltip.text(d.size + " users");
-    //     // highlight the current path
-    //     let selection = d3.select(this).transition("tooltip").duration(400);
-    //     selection.select("path")
-    //       .style("stroke-width", 3)
-    //       // .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-    //       .style("stroke-opacity", 1);
-    //   })
-    //   .on("mousemove", function () {
-    //     tooltip.style("left", (d3.event.pageX) + "px")
-    //       .style("top", (d3.event.pageY - 28) + "px");
-    //   })
-    //   .on("mouseout", function (d, i) {
-    //     tooltip.transition().duration(400).style("opacity", 0);
-    //     let selection = d3.select(this).transition("tooltip").duration(400);
-    //     selection.select("path")
-    //       .style("stroke-width", 0)
-    //       // .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
-    //       .style("stroke-opacity", 0);
-    //   });
-
-
-    //rings
-    const ringsElement = this.root.querySelector('#rings');
-    const ringsSvg =
-      d3.select(ringsElement)
-        .append('svg')
-        .attr('width', 800)
-        .attr('height', 380)
-        .datum(sets)
-        .call(chart);
-
-    ringsSvg
-      .selectAll('.venn-circle path')
+    interactiveSvg.selectAll('g')
       .data(sets)
-      .style('fill-opacity', 0)
-      .style('stroke-width', 10)
-      .style('stroke-opacity', 0.5)
-      .style('stroke', function (d, i) {
-        return colorScheme[i];
-      });
-    ringsSvg
-      .selectAll('#rings .venn-circle text')
-      .style('fill', function (d, i) {
-        return colorScheme[i];
+      .on('mouseover', function (e, d) {
+        // sort all the areas relative to the current item
+        venn.sortAreas(interactiveSvg, d);
+
+        // Display a tooltip with the current size
+        tooltip.transition().duration(100).style('opacity', .9);
+        console.log('d.size', d.size)
+        tooltip.text(d.size + ' genes');
+
+        // highlight the current path
+        let selection = d3.select(this).transition('tooltip').duration(400);
+        selection.select('path')
+          .style('fill-opacity', d.sets.length == 1 ? .4 : .1)
+          .style('stroke-opacity', 1);
       })
-      .style('font-size', '24px')
-      .style('font-weight', '100');
-
-    //inverted Venn diagram
-    const invertedElement = this.root.querySelector('#inverted');
-    const invertedSvg =
-      d3.select(invertedElement)
-        .append('svg')
-        .attr('width', 800)
-        .attr('height', 400)
-        .datum(sets)
-        .call(chart);
-    invertedSvg
-      .selectAll('#inverted .venn-circle path')
-      .style('fill-opacity', .8);
-    invertedSvg
-      .selectAll('#inverted text')
-      .style('fill', '#FFF');
-
-    //monochart
-    const monoElement = this.root.querySelector('#mono');
-    const monoSvg =
-      d3.select(monoElement)
-        .append('svg')
-        .attr('width', 800)
-        .attr('height', 400)
-        .datum(sets)
-        .call(chart);
-
-    monoSvg
-      .selectAll('#mono .venn-circle path')
-      .style('fill-opacity', 0)
-      .style('stroke-width', 2)
-      .style('stroke', '#444');
-    monoSvg
-      .selectAll('#mono text')
-      .style('fill', '#444');
+      .on('mousemove', function () {
+        tooltip.style('left', (d3.event.pageX) + 'px')
+          .style('top', (d3.event.pageY - 28) + 'px');
+      })
+      .on('mouseout', function (d, i) {
+        tooltip.transition().duration(400).style('opacity', 0);
+        let selection = d3.select(this).transition('tooltip').duration(400);
+        selection.select('path')
+          // .style('fill-opacity', .5)
+          // .style('fill-opacity', d.sets.length == 1 ? .25 : .0)
+          .style('stroke-opacity', 0);
+      });
   }
 }
