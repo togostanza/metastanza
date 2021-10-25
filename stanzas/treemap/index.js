3b6937f7db1b7f75f05826e25c96ad6462fa45f1
Anton <anton@penqe.com>
2021-10-25 19:10:15 +0900

    no message

--- stanzas/treemap/index.js
+++ stanzas/treemap/index.js
@@ -1,207 +1,101 @@
 import Stanza from "togostanza/stanza";
-import vegaEmbed from "vega-embed";
-import loadData from "@/lib/load-data";
+import * as d3 from "d3";
 import {
   downloadSvgMenuItem,
   downloadPngMenuItem,
   appendCustomCss,
 } from "@/lib/metastanza_utils.js";
 
-export default class Treemap extends Stanza { //TODO ここのclass名はスタンザ名にする必要があります
+export default class TreeMapStanza extends Stanza {
   menu() {
     return [
-      downloadSvgMenuItem(this, "tree"),
-      downloadPngMenuItem(this, "tree"),
+      downloadSvgMenuItem(this, "treeMapstanza"),
+      downloadPngMenuItem(this, "treeMapstanza"),
     ];
   }
 
   async render() {
-    appendCustomCss(this, this.params["custom-css-url"]);
-
     const css = (key) => getComputedStyle(this.element).getPropertyValue(key);
+    // const url =
+    //   "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_dendrogram_full.json";
 
-    //width,height,padding
-    const width = 960;
-    const height = 500;
-    const padding = 2.5;
+    // const json = await fetch(url);
 
-    //TODO this.paramsが記載されていながらも、metastanza.jsonに対応する設定の記述がなかったためビルドできていないようでした
-    //TODO こちらの設定は現在は使わないですが、後ほど設定するのでひとまずコメントアウトしております
-    // //data
-    // const labelVariable = this.params["label"]; //"name"
-    // const parentVariable = this.params["parent-node"]; //"parent"
-    // const idVariable = this.params["node"]; //"id-variable"
+    // const dataSet = await json.json();
 
-    // const values = await loadData(
-    //   this.params["data-url"],
-    //   this.params["data-type"]
-    // );
-
-    const signals = [
-      {
-        name: "layout",
-        value: "squarify",
-        bind: {
-          input: "select",
-          options: [
-            "squarify",
-            "binary",
-            "slicedice"
-          ]
-        }
-      },
-      {
-        name: "aspectRatio",
-        value: "1.6",
-        bind: {input: "range", min: 1, max: 5, step: 0.1}
-      },
-    ];
-
-    const data = [
-      {
-        name: "tree",
-        // url: "data/flare.json", //TODO 相対パスになっています。同ディレクトリ内にデータがない場合は、絶対パスで記述する必要があります。
-        url: "https://vega.github.io/vega/data/flare.json", //TODO 絶対パスに変更しました。
-        transform: [
-          {
-            type: "stratify",
-            key: "id",
-            parentKey: "parent",
-          },
-          {
-            type: "treemap",
-            field: "size",
-            sort: { field: "value" },
-            round: true,
-            method: { signal: "layout" },
-            ratio: { signal: "aspectRatio" },
-            size: [{ signal: "width" }, { signal: "height" }]
-          },
-        ],
-      },
-      {
-        name: "nodes",
-        source: "tree",
-        transform: [
-          { 
-            type: "filter",
-            expr: "datum.children" 
-          },
-        ],
-      },
-      {
-        name: "leaves",
-        source: "tree",
-        transform: [
-          { 
-            type: "filter",
-            expr: "!datum.children" 
-          },
-        ],
-      },
-    ];
-
-    //scales
-    const scales = [
-      {
-        name: "color",
-        type: "ordinal",
-        domain: { data: "nodes", field: "name" },
-        range: [
-          "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d",
-          "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476",
-          "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc",
-          "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9"
-        ],
-      },
-      {
-        name: "size",
-        type: "ordinal",
-        domain: [0, 1, 2, 3],
-        range: [256, 28, 20, 14]
-      },
-      {
-        name: "opacity",
-        type: "ordinal",
-        domain: [0, 1, 2, 3],
-        range: [0.15, 0.5, 0.8, 1.0]
-      },
-    ];
-
-    //marks
-    const marks = [
-      {
-        type: "rect",
-        from: { data: "nodes" },
-        interactive: false,
-        encode: {
-          enter: {
-            fill: { scale: "color", field: "name" },
-          },
+    // console.log(dataSet);
+    const data = {
+      name: "A",
+      children: [
+        { name: "B", value: 25 },
+        {
+          name: "C",
+          children: [
+            { name: "D", value: 10 },
+            { name: "E", value: 15 },
+            { name: "F", value: 10 },
+          ],
         },
-        update: {
-          x: { field: "x0" },
-          y: { field: "y0" },
-          x2: { field: "x1" },
-          y2: { field: "y1" },
+        { name: "G", value: 15 },
+        {
+          name: "H",
+          children: [
+            { name: "I", value: 20 },
+            { name: "J", value: 10 },
+          ],
         },
-      },
-      {
-        type: "rect",
-        from: { data: "leaves" },
-        encode: {
-          enter: {
-            stroke: { value: "#fff" },
-          },
-          update: {
-            x: { field: "x0" },
-            y: { field: "y0" },
-            x2: { field: "x1" },
-            y2: { field: "y1" },
-            fill: { value: "transparent" },
-          },
-          hover: {
-            fill: { value: "red" },
-          },
-        },
-      },
-      {
-        type: "text",
-        from: { data: "nodes" },
-        interactive: false,
-        encode: {
-          enter: {
-            font: { value: "Helvetica Neue, Arial" },
-            align: { value: "center"},
-            baseline: { value: "middle"},
-            fill: { value: "#000"},
-            text: { field: "name"},
-            fontSize: { scale: "size", field: "depth"},
-            fillOpacity: {scale: "opacity", field: "depth"},
-          },
-          update: {
-            x: { signal: "0.5 * (datum.x0 + datum.x1)" },
-            y: { signal: "0.5 * (datum.y0 + datum.y1)" },
-          },
-        },
-      },
-    ];
-
-    const spec = {
-      $schema: "https://vega.github.io/schema/vega/v5.json",
-      width,
-      height,
-      padding,
-      signals,
-      data,
-      scales,
-      marks,
+        { name: "K", value: 10 },
+      ],
     };
+    this.renderTemplate({ template: "stanza.html.hbs" });
 
-    const el = this.root.querySelector("main");
-    const opts = {
-      renderer: "svg",
-    };
-    await vegaEmbed(el, spec, opts);
+    let treeMapElement = this.root.querySelector("#treemap");
+
+    const width = 300;
+    const height = 200;
+    draw(treeMapElement, data, width, height);
   }
+}
+
+function draw(el, dataset, width, height) {
+  let svg = d3
+    .select(el)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  let root = d3.hierarchy(dataset).sum((d) => d.value);
+  let treeMapLayout = d3.treemap().size([width, height]);
+  treeMapLayout(root);
+
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")")
+
+    .append("rect")
+    .attr("class", "tile")
+    .attr("width", (d) => d.x1 - d.x0)
+    .attr("height", (d) => d.y1 - d.y0)
+    .style("fill", (d) => {
+      while (d.depth > 1) d = d.parent;
+      return d3.schemeCategory10[parseInt(d.value % 7)];
+    })
+    .attr("data-name", (d) => d.data.name)
+    .attr("data-value", (d) => d.data.value);
+
+  svg
+    .selectAll("g")
+    .append("text")
+    .attr("text-anchor", "start")
+    .attr("x", 5)
+    .attr("dy", 30)
+    .attr("font-size", "60%")
+    .attr("color", "white")
+    .text((d) => {
+      return d.data.name + ":" + d.value;
+    });
 }
