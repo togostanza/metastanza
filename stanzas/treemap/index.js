800163cae61e54b38d29c1b30e95f51d1d1b15cc
Anton <nokker@gmail.com>
2021-10-27 16:56:30 +0900

    Text wrapping added

--- stanzas/treemap/index.js
+++ stanzas/treemap/index.js
@@ -1,12 +1,13 @@
 import Stanza from "togostanza/stanza";
 import * as d3 from "d3";
-import * as d3Collection from "d3-collection";
 import uid from "./uid";
-import data from "./sampleData2";
+//import data from "./sampleData2";
+import loadData from "@/lib/load-data";
 
 import {
   downloadSvgMenuItem,
   downloadPngMenuItem,
+  appendCustomCss,
 } from "@/lib/metastanza_utils.js";
 
 export default class TreeMapStanza extends Stanza {
@@ -18,25 +19,41 @@ export default class TreeMapStanza extends Stanza {
   }
 
   async render() {
+    appendCustomCss(this, this.params["custom-css-url"]);
+
+    const width = this.params["width"];
+    const height = this.params["height"];
+    const colorScale = this.params["color-scale"];
+    const data = await loadData(
+      this.params["data-url"],
+      this.params["data-type"]
+    );
+
     this.renderTemplate({ template: "stanza.html.hbs" });
 
-    let treeMapElement = this.root.querySelector("#treemap");
-
-    const width = 300;
-    const height = 200;
-
-    data.push({ id: 0, value: "", label: "" });
+    //Add root element if there are more than one elements without parent. D3 cannot precess data with more than one root elements
+    let rootElemIndexes = [];
     for (let i = 0; i < data.length - 1; i++) {
       if (!data[i]?.parent) {
-        data[i].parent = 0;
+        rootElemIndexes.push(i);
       }
     }
+    if (rootElemIndexes.length > 1) {
+      data.push({ id: -1, value: "", label: "" });
 
-    draw(treeMapElement, data, width, height);
+      rootElemIndexes.forEach((index) => {
+        data[index].parent = -1;
+      });
+    }
+
+    const treeMapElement = this.root.querySelector("#treemap");
+
+    draw(treeMapElement, data, width, height, colorScale);
   }
 }
 
-function draw(el, dataset, width, height) {
+function draw(el, dataset, width, height, colorScale) {
+  // create nested structure by item.parent
   const nested = d3
     .stratify()
     .id(function (d) {
@@ -48,7 +65,12 @@ function draw(el, dataset, width, height) {
 
   const x = d3.scaleLinear().rangeRound([0, width]);
   const y = d3.scaleLinear().rangeRound([0, height]);
+
+  // make path-like string for node
   const name = (d) => {
+    if (d.data.data.id === -1) {
+      return "/";
+    }
     return d
       .ancestors()
       .reverse()
@@ -59,7 +81,8 @@ function draw(el, dataset, width, height) {
   };
 
   const format = d3.format(",d");
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+  const color = d3.scaleOrdinal(d3[colorScale]);
 
   function tile(node, x0, y0, x1, y1) {
     d3.treemapBinary(node, 0, 0, width, height);
@@ -73,8 +96,8 @@ function draw(el, dataset, width, height) {
 
   const treemap = (data) =>
     d3.treemap().tile(tile)(
-      d3.hierarchy(data).sum((d) => 1)
-      //.sort((a, b) => b?.children?.length || 0 - a?.children?.length || 0)
+      d3.hierarchy(data).sum((d) => d?.children?.length || 1)
+      //.sort((a, b) => b?.children?.length || 1 - a?.children?.length || 1)
     );
 
   const svg = d3
@@ -86,41 +109,6 @@ function draw(el, dataset, width, height) {
   let group = svg.append("g").call(render, treemap(nested));
 
   function render(group, root) {
-    // function to wrap text
-    function wrap(text, width) {
-      text.each(function () {
-        var text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
-          line = [],
-          lineNumber = 0,
-          lineHeight = 1.1, // ems
-          x = text.attr("x"),
-          y = text.attr("y"),
-          dy = 0, //parseFloat(text.attr("dy")),
-          tspan = text
-            .text(null)
-            .append("tspan")
-            .attr("x", x)
-            .attr("y", y)
-            .attr("dy", dy + "em");
-        while ((word = words.pop())) {
-          line.push(word);
-          tspan.text(line.join(" "));
-          if (tspan.node().getComputedTextLength() > width) {
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text
-              .append("tspan")
-              .attr("x", x)
-              .attr("y", y)
-              .attr("dy", ++lineNumber * lineHeight + dy + "em")
-              .text(word);
-          }
-        }
-      });
-    }
     const dMax = d3.max(root, (d) => d?.children?.length || 1);
     const dMin = d3.min(root, (d) => d?.children?.length || 1);
 
@@ -136,14 +124,22 @@ function draw(el, dataset, width, height) {
 
     node
       .append("title")
-      .text((d) => `${name(d)}\n${format(d?.children?.length || 0)}`);
+      .text((d) =>
+        d === root
+          ? ""
+          : `${name(d)}\n${
+              d?.children
+                ? format(d?.children?.length)
+                : d.data.data.description
+            }`
+      );
 
     node
       .append("rect")
       .attr("id", (d) => (d.leafUid = uid("leaf")).id)
       .attr("fill", (d) =>
         d === root ? "#fff" : color((d.value - dMin) / (dMax - dMin))
-      ) //d.children ? "#ccc" : "#ddd"))
+      )
       .attr("stroke", "#fff");
 
     node
@@ -152,13 +148,13 @@ function draw(el, dataset, width, height) {
       .append("use")
       .attr("href", (d) => d.leafUid.href);
 
-    node
-
+    const txt = node
       .append("text")
       .attr("clip-path", (d) => d.clipUid)
       .attr("font-weight", (d) => (d === root ? "bold" : null))
+
       .attr("y", (d) => `${d.y0 + 10}px`)
-      .attr("x", "0.1em")
+      .attr("x", "0.5em")
       //.selectAll("tspan")
       // .data((d) => {
       //   return d === root ? name(d) : d.data.data.label;
@@ -177,25 +173,65 @@ function draw(el, dataset, width, height) {
       // .attr("font-weight", (d, i, nodes) =>
       //   i === nodes.length - 1 ? "normal" : null
       // )
-      .text((d) => d.data.data.label)
-      .append("tspan")
-      .attr("dy", "1.1em")
-      .attr("x", "0.1em")
       .text((d) => {
-        if (d === root) return "";
-        else {
-          if (d?.children?.length) {
-            return d?.children?.length;
-          }
-          return "";
+        if (d === root) {
+          return name(d);
+        } else if (d?.children?.length) {
+          return `${d.data.data.label} : ${d?.children?.length}`;
         }
+        return `${d.data.data.label}`;
       });
-
-    //.call(wrap, (d) => d.x1 - d.x0);
 
     group.call(position, root);
   }
 
+  function wrap() {
+    const text = d3.select(this).selectAll("text");
+
+    text.each(function () {
+      let text = d3.select(this);
+
+      let rectWidth = d3
+        .select(text.node().parentNode)
+        .select("rect")
+        .node()
+        .getAttribute("width");
+
+      //get to know if this is a root element (white bar at the top)
+      const isRoot = Math.round(rectWidth) === Math.round(width);
+
+      let words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        x = text.attr("x") || 0,
+        y = text.attr("y") || 0,
+        dy = 0,
+        tspan = text
+          .text(null)
+          .append("tspan")
+          .attr("x", x)
+          .attr("y", y)
+          .attr("dy", dy + "em");
+      console.log(words);
+      while ((word = words.pop())) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > rectWidth) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text
+            .append("tspan")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+            .text(word);
+        }
+      }
+    });
+  }
   //place elements according to data
   function position(group, root) {
     group
@@ -225,7 +261,8 @@ function draw(el, dataset, width, height) {
           .transition(t)
           .attrTween("opacity", () => d3.interpolate(0, 1))
           .call(position, d)
-      );
+      )
+      .on("end", wrap);
   }
 
   // When zooming out, draw the old nodes on top, and fade them out.
@@ -246,6 +283,7 @@ function draw(el, dataset, width, height) {
           .attrTween("opacity", () => d3.interpolate(1, 0))
           .call(position, d)
       )
-      .call((t) => group1.transition(t).call(position, d.parent));
+      .call((t) => group1.transition(t).call(position, d.parent))
+      .on("end", wrap);
   }
 }
