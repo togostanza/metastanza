381305e369b4f60dbd4b2ac18ed6e8a545fc9940
Anton Zhuravlev <nokker@gmail.com>
2021-11-19 11:31:06 +0900

    package-lock.json fix (delete all insides and npm i again)

--- stanzas/treemap/uid.js
+++ stanzas/treemap/uid.js
@@ -1,7 +1,7 @@
 var count = 0;
 
 export default function (name) {
-  return new Id("O-" + (name == null ? "" : name + "-") + ++count);
+  return new Id("O-" + (name === null ? "" : name + "-") + ++count);
 }
 
 function Id(id) {
