e2105991fdcb25dbca9b52a9cfba8a7259886dc4
darashi <darashi@users.noreply.github.com>
2021-12-10 04:51:00 +0000

    deploy: 08a3aeafbf201f630fa33d92dfc4b24691e7b374

--- tree.js
+++ tree.js
@@ -1,4 +1,4 @@
-import { S as Stanza, d as downloadSvgMenuItem, a as downloadPngMenuItem, g as appendCustomCss, h as defineStanzaElement } from './index-f66d4cf6.js';
+import { S as Stanza, d as downloadSvgMenuItem, a as downloadPngMenuItem, b as downloadJSONMenuItem, c as downloadCSVMenuItem, e as downloadTSVMenuItem, f as copyHTMLSnippetToClipboardMenuItem, g as appendCustomCss, h as defineStanzaElement } from './index-f66d4cf6.js';
 import { e as embed } from './vega-embed.module-0d1b15ca.js';
 import { l as loadData } from './load-data-8921580f.js';
 
@@ -7,6 +7,10 @@ class Tree extends Stanza {
     return [
       downloadSvgMenuItem(this, "tree"),
       downloadPngMenuItem(this, "tree"),
+      downloadJSONMenuItem(this, "tree", this._data),
+      downloadCSVMenuItem(this, "tree", this._data),
+      downloadTSVMenuItem(this, "tree", this._data),
+      copyHTMLSnippetToClipboardMenuItem(this),
     ];
   }
 
@@ -29,6 +33,7 @@ class Tree extends Stanza {
       this.params["data-url"],
       this.params["data-type"]
     );
+    this._data = values;
 
     const signals = [
       {
