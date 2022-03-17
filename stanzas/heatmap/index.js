0ab84ded362f1d3d8fb318ab7efdd10eca9c69ce
MarleenEliza <marleen.dijkman@outlook.com>
2022-03-17 18:11:19 +0900

    fix lint

--- stanzas/heatmap/index.js
+++ stanzas/heatmap/index.js
@@ -36,11 +36,11 @@ export default class Heatmap extends Stanza {
     const yLabelAngle = this.params["y-label-angle"] || 0;
 
     // set the dimensions and margins of the graph
-        const margin = {
-      bottom: +this.css("--togostanza-font-size") + tickSize  +  10,
-      left: +this.css("--togostanza-font-size") + tickSize  + 10,
+    const margin = {
+      bottom: +this.css("--togostanza-font-size") + tickSize + 10,
+      left: +this.css("--togostanza-font-size") + tickSize + 10,
     };
-      const width = this.params["width"] - margin.left,
+    const width = this.params["width"] - margin.left,
       height = this.params["height"] - margin.bottom;
 
     // remove svg element whenthis.params updated
@@ -50,43 +50,39 @@ export default class Heatmap extends Stanza {
       .select(el)
       .append("svg")
       .attr("width", width + margin.left)
-      .attr("height", height + margin.bottom)
-    
+      .attr("height", height + margin.bottom);
+
     const graphArea = svg
       .append("g")
-      .attr('class', 'chart')
-      .attr("transform", `translate(${margin.left}, 0 )`)
+      .attr("class", "chart")
+      .attr("transform", `translate(${margin.left}, 0 )`);
 
     const rows = [...new Set(dataset.map((d) => d.group))];
     const columns = [...new Set(dataset.map((d) => d.variable))];
 
-    const x = d3
-      .scaleBand()
-      .domain(rows)
-      .range([0, width])
-      .padding(0.01)
+    const x = d3.scaleBand().domain(rows).range([0, width]).padding(0.01);
 
     const xAxisGenerator = d3
       .axisBottom(x)
       .tickSizeOuter(0)
-      .tickSizeInner(tickSize)
+      .tickSizeInner(tickSize);
 
     const y = d3.scaleBand().range([height, 0]).domain(columns).padding(0.01);
     const yAxisGridGenerator = d3
       .axisLeft(y)
       .tickSizeOuter(0)
-      .tickSizeInner(tickSize)
+      .tickSizeInner(tickSize);
 
     graphArea
       .append("g")
-      .attr('class', 'x-axis')
+      .attr("class", "x-axis")
       .attr("transform", `translate(0, ${height})`)
       .transition()
       .duration(200)
       .call(xAxisGenerator)
       .selectAll("text")
-      .attr("transform", `rotate(${xLabelAngle})`)
-      
+      .attr("transform", `rotate(${xLabelAngle})`);
+
     graphArea
       .append("g")
       .classed("class", "y-axis")
@@ -94,8 +90,7 @@ export default class Heatmap extends Stanza {
       .duration(200)
       .call(yAxisGridGenerator)
       .selectAll("text")
-      .attr("transform", `rotate(${yLabelAngle})`)
-      
+      .attr("transform", `rotate(${yLabelAngle})`);
 
     if (!this.params["show-domains"]) {
       svg.selectAll(".domain").remove();
@@ -109,7 +104,7 @@ export default class Heatmap extends Stanza {
       .attr("font-family", this.css("--togostanza-font-family"))
       .attr("fill", this.css("--togostanza-font-color"))
       .attr("font-size", Number(this.css("--togostanza-font-size")))
-      .attr("font-weight", this.css("--togostanza-font-weight"))
+      .attr("font-weight", this.css("--togostanza-font-weight"));
 
     const myColor = d3
       .scaleLinear()
@@ -154,7 +149,7 @@ export default class Heatmap extends Stanza {
   ) {
     const [min, max] = [Math.min(...values), Math.max(...values)];
     return [...Array(steps).keys()].map((i) => {
-      const n = Math.round(min + (i) * ((max - min) / steps));
+      const n = Math.round(min + i * ((max - min) / steps));
       return {
         label: n,
         color: color(n),
