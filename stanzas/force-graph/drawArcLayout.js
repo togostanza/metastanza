f9b3e28407a3c88ffefc36c00f7e843081fc52a7
Anton <nokker@gmail.com>
2022-03-17 16:12:03 +0900

    arcLayout done

--- stanzas/force-graph/drawArcLayout.js
+++ stanzas/force-graph/drawArcLayout.js
@@ -1,43 +1,79 @@
 import * as d3 from "d3";
 
-export default function (svg, nodes, edges) {
+export default function (svg, nodes, edges, params) {
   const nodesC = JSON.parse(JSON.stringify(nodes));
   const edgesC = JSON.parse(JSON.stringify(edges));
 
-  const MARGIN = {
-    TOP: 10,
-    BOTTOM: 10,
-    LEFT: 10,
-    RIGHT: 10,
-  };
-  const height = svg.attr("height");
-  const width = svg.attr("width");
+  const {
+    width,
+    height,
+    MARGIN,
+    color,
+    symbols,
+    nodeSizeParams,
+    edgeWidthParams,
+    labelsParams,
+  } = params;
 
   // const HEIGHT = height - MARGIN.TOP - MARGIN.BOTTOM;
   const WIDTH = width - MARGIN.LEFT - MARGIN.RIGHT;
-
-  const color = this[Symbol.for("color")];
-  const sizeScale = this[Symbol.for("sizeScale")];
-  const count = this[Symbol.for("count")];
-  const edgeSym = Symbol.for("nodeAdjEdges");
 
   const nodeHash = {};
   nodesC.forEach((node) => {
     nodeHash[node.id] = node;
   });
 
+  let edgeWidthScale;
+  if (edgeWidthParams.basedOn === "dataKey") {
+    edgeWidthScale = d3
+      .scaleLinear()
+      .domain(d3.extent(edgesC, (d) => d[edgeWidthParams.dataKey]))
+      .range([edgeWidthParams.minWidth, edgeWidthParams.maxWidth]);
+  } else {
+    edgeWidthScale = () => edgeWidthParams.fixedWidth;
+  }
+
   edgesC.forEach((edge) => {
-    edge.weight = parseInt(edge.value);
-    edge.sourceNode = nodeHash[edge.source];
-    edge.targetNode = nodeHash[edge.target];
+    edge[symbols.edgeWidthSym] = edgeWidthScale(
+      parseFloat(edge[edgeWidthParams.dataKey])
+    );
+    edge[symbols.sourceNodeSym] = nodeHash[edge.source];
+    edge[symbols.targetNodeSym] = nodeHash[edge.target];
   });
 
   nodesC.forEach((node) => {
     const adjEdges = edgesC.filter((edge) => {
-      return edge.sourceNode === node || edge.targetNode === node;
+      return (
+        edge[symbols.sourceNodeSym] === node ||
+        edge[symbols.targetNodeSym] === node
+      );
     });
-    node[edgeSym] = adjEdges;
+    node[symbols.edgeSym] = adjEdges;
   });
+
+  let nodeSizeScale;
+  if (nodeSizeParams.basedOn === "dataKey") {
+    nodeSizeScale = d3
+      .scaleLinear()
+      .domain(d3.extent(nodesC, (d) => d[nodeSizeParams.dataKey]))
+      .range([nodeSizeParams.minSize, nodeSizeParams.maxSize]);
+    nodesC.forEach((node) => {
+      node[symbols.nodeSizeSym] = nodeSizeScale(node[nodeSizeParams.dataKey]);
+    });
+  } else if (nodeSizeParams.basedOn === "edgesNumber") {
+    nodeSizeScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(nodesC, (d) => d[symbols.edgeSym].length)])
+      .range([nodeSizeParams.minSize, nodeSizeParams.maxSize]);
+
+    nodesC.forEach((node) => {
+      node[symbols.nodeSizeSym] = nodeSizeScale(node[symbols.edgeSym].length);
+    });
+  } else {
+    nodesC.forEach((node) => {
+      node[symbols.nodeSizeSym] = nodeSizeParams.fixedSize;
+    });
+  }
 
   const pointScale = d3
     .scalePoint()
@@ -55,54 +91,68 @@ export default function (svg, nodes, edges) {
     .enter()
     .append("path")
     .attr("class", "link")
-    .style("stroke-width", (d) => d.weight * 0.5)
+    .style("stroke-width", (d) => d[symbols.edgeWidthSym])
     .style("stroke", (d) => {
-      return color(d.sourceNode.id);
+      return color(d[symbols.sourceNodeSym].id);
     })
     .style("stroke-opacity", 0.25)
     .style("fill", "none")
-    .attr("d", (d) => arc(d))
-    .attr("marker-end", "url(#arrow)");
+    .attr("d", (d) => arc(d));
 
-  const circles = arcG
-    .selectAll("circle")
+  const nodeGroups = arcG
+    .selectAll("g")
     .data(nodesC)
     .enter()
+    .append("g")
+    .attr("class", "node-group")
+    .attr("transform", (d) => `translate(${pointScale(d.id)},0)`);
+
+  const circles = nodeGroups
     .append("circle")
     .attr("class", "node")
     .attr("fill", (d) => color(d.id))
-    .attr("r", (d) => sizeScale(count[d.id]))
-    .attr("cx", (d) => pointScale(d.id))
+    .attr("r", (d) => d[symbols.nodeSizeSym])
     .attr("data-tooltip", (d) => d.id);
 
-  circles.on("mouseover", function (e, d) {
+  if (labelsParams.show) {
+    const labels = nodeGroups
+      .append("text")
+      .text((d) => d.id)
+      .attr("alignment-baseline", "middle")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -labelsParams.margin)
+      .attr("class", "label");
+  }
+
+  nodeGroups.on("mouseover", function (e, d) {
     // highlight current node
     d3.select(this).classed("active", true);
-
     // fade out all other nodes, highlight a little connected ones
-    circles
+    nodeGroups
       .classed("fadeout", (p) => d !== p)
       .classed("half-active", (p) => {
         return (
           p !== d &&
-          d[edgeSym].some(
-            (edge) => edge.sourceNode === p || edge.targetNode === p
+          d[symbols.edgeSym].some(
+            (edge) =>
+              edge[symbols.sourceNodeSym] === p ||
+              edge[symbols.targetNodeSym] === p
           )
         );
       });
-
     // fadeout not connected edges, highlight connected ones
     links
-      .classed("fadeout", (p) => !d[edgeSym].includes(p))
-      .classed("active", (p) => d[edgeSym].includes(p));
+      .classed("fadeout", (p) => !d[symbols.edgeSym].includes(p))
+      .classed("active", (p) => d[symbols.edgeSym].includes(p));
   });
 
-  circles.on("mouseleave", function () {
-    circles
+  nodeGroups.on("mouseleave", function () {
+    links
       .classed("active", false)
       .classed("fadeout", false)
       .classed("half-active", false);
-    links
+    nodeGroups
       .classed("active", false)
       .classed("fadeout", false)
       .classed("half-active", false);
@@ -110,11 +160,15 @@ export default function (svg, nodes, edges) {
 
   function arc(d) {
     const draw = d3.line().curve(d3.curveBasis);
-    const sourceX = pointScale(d.sourceNode.id);
-    const targetX = pointScale(d.targetNode.id);
-    console.log(sourceX - targetX);
+    const sourceX = pointScale(d[symbols.sourceNodeSym].id);
+    const targetX = pointScale(d[symbols.targetNodeSym].id);
     const midX = (sourceX + targetX) / 2;
-    const midY = (sourceX - targetX) / 3;
+    let midY;
+    if (sourceX < targetX) {
+      midY = -(height / 12 + (targetX - sourceX) / 3);
+    } else {
+      midY = -(height / 20 + (sourceX - targetX) / 3);
+    }
     return draw([
       [sourceX, 0],
       [midX, midY],
--- stanzas/force-graph/index.js
+++ stanzas/force-graph/index.js
@@ -51,8 +51,6 @@ export default class ForceGraph extends Stanza {
     const nodes = values.nodes;
     const edges = values.links;
 
-    await setTimeout(() => {}, 100);
-
     const count = {};
     for (const element of edges) {
       if (count[element.target]) {
@@ -67,8 +65,17 @@ export default class ForceGraph extends Stanza {
       }
     }
 
+    // TODO delete next line
     this[Symbol.for("count")] = count;
 
+    const MARGIN = {
+      TOP: 10,
+      BOTTOM: 10,
+      LEFT: 10,
+      RIGHT: 10,
+    };
+
+    // TODO delete next lines
     // Setting node size scale
     const sizeScale = d3.scaleSqrt([0, d3.max(Object.values(count))], [4, 16]);
     this[Symbol.for("sizeScale")] = sizeScale;
@@ -81,7 +88,7 @@ export default class ForceGraph extends Stanza {
     const color = d3.scaleOrdinal().range(togostanzaColors);
     this[Symbol.for("color")] = color;
 
-    const root = this.root.querySelector(":scope > div");
+    const root = this.root.querySelector("main");
     const el = this.root.getElementById("force-graph");
 
     const existingSvg = root.getElementsByTagName("svg")[0];
@@ -98,6 +105,46 @@ export default class ForceGraph extends Stanza {
       this.tooltip = new ToolTip();
       root.append(this.tooltip);
     }
+
+    const edgeSym = Symbol.for("nodeAdjEdges");
+    const edgeWidthSym = Symbol.for("edgeWidth");
+    const sourceNodeSym = Symbol.for("sourceNode");
+    const targetNodeSym = Symbol.for("targetNode");
+    const nodeSizeSym = Symbol.for("nodeSize");
+
+    const symbols = {
+      edgeSym,
+      edgeWidthSym,
+      sourceNodeSym,
+      targetNodeSym,
+      nodeSizeSym,
+    };
+    const nodeSizeParams = {
+      basedOn: "edgesNumber",
+      dataKey: "value",
+      fixedSize: 10,
+      minSize: 5,
+      maxSize: 15,
+    };
+    const edgeWidthParams = {
+      basedOn: "dataKey",
+      dataKey: "value",
+      fixedWidth: 2,
+      minWidth: 1,
+      maxWidth: 10,
+    };
+
+    const params = {
+      MARGIN,
+      width,
+      height,
+      svg,
+      color,
+      symbols,
+      nodeSizeParams,
+      edgeWidthParams,
+      labelsParams: { margin: 10, show: true },
+    };
 
     // svg
     //   .append("defs")
@@ -118,7 +165,7 @@ export default class ForceGraph extends Stanza {
         drawFoecLayout.call(this, svg, nodes, edges);
         break;
       case "arc":
-        drawArcLayout.call(this, svg, nodes, edges);
+        drawArcLayout(svg, nodes, edges, params);
         break;
       case "grid":
         drawGridLayout.call(this, svg, nodes, edges);
