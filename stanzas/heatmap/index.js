04fab058ff19561e6677a6088507e8c19c50c67f
MarleenEliza <marleen.dijkman@outlook.com>
2022-03-17 18:09:35 +0900

    prepare for prod

--- stanzas/heatmap/index.js
+++ stanzas/heatmap/index.js
@@ -31,15 +31,17 @@ export default class Heatmap extends Stanza {
     this.draw(chartElement, data);
   }
   async draw(el, dataset) {
+    const tickSize = +this.css("--togostanza-tick-size") || 0;
+    const xLabelAngle = this.params["x-label-angle"] || 0;
+    const yLabelAngle = this.params["y-label-angle"] || 0;
+
     // set the dimensions and margins of the graph
-    const margin = {
-        top: this.params["top"],
-        right: this.params["right"],
-        bottom: this.params["bottom"],
-        left: this.params["left"],
-      },
-      width = this.params["width"] - margin.left - margin.right,
-      height = this.params["height"] - margin.top - margin.bottom;
+        const margin = {
+      bottom: +this.css("--togostanza-font-size") + tickSize  +  10,
+      left: +this.css("--togostanza-font-size") + tickSize  + 10,
+    };
+      const width = this.params["width"] - margin.left,
+      height = this.params["height"] - margin.bottom;
 
     // remove svg element whenthis.params updated
     d3.select(el).select("svg").remove();
@@ -47,22 +49,53 @@ export default class Heatmap extends Stanza {
     const svg = d3
       .select(el)
       .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left)
+      .attr("height", height + margin.bottom)
+    
+    const graphArea = svg
       .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+      .attr('class', 'chart')
+      .attr("transform", `translate(${margin.left}, 0 )`)
 
     const rows = [...new Set(dataset.map((d) => d.group))];
     const columns = [...new Set(dataset.map((d) => d.variable))];
 
-    const x = d3.scaleBand().range([0, width]).domain(rows).padding(0.01);
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
+    const x = d3
+      .scaleBand()
+      .domain(rows)
+      .range([0, width])
+      .padding(0.01)
+
+    const xAxisGenerator = d3
+      .axisBottom(x)
+      .tickSizeOuter(0)
+      .tickSizeInner(tickSize)
 
     const y = d3.scaleBand().range([height, 0]).domain(columns).padding(0.01);
-    svg.append("g").call(d3.axisLeft(y));
+    const yAxisGridGenerator = d3
+      .axisLeft(y)
+      .tickSizeOuter(0)
+      .tickSizeInner(tickSize)
+
+    graphArea
+      .append("g")
+      .attr('class', 'x-axis')
+      .attr("transform", `translate(0, ${height})`)
+      .transition()
+      .duration(200)
+      .call(xAxisGenerator)
+      .selectAll("text")
+      .attr("transform", `rotate(${xLabelAngle})`)
+      
+    graphArea
+      .append("g")
+      .classed("class", "y-axis")
+      .transition()
+      .duration(200)
+      .call(yAxisGridGenerator)
+      .selectAll("text")
+      .attr("transform", `rotate(${yLabelAngle})`)
+      
 
     if (!this.params["show-domains"]) {
       svg.selectAll(".domain").remove();
@@ -71,12 +104,12 @@ export default class Heatmap extends Stanza {
       svg.selectAll(".tick line").remove();
     }
 
-    svg
+    graphArea
       .selectAll("text")
       .attr("font-family", this.css("--togostanza-font-family"))
       .attr("fill", this.css("--togostanza-font-color"))
       .attr("font-size", Number(this.css("--togostanza-font-size")))
-      .attr("font-weight", this.css("--togostanza-font-weight"));
+      .attr("font-weight", this.css("--togostanza-font-weight"))
 
     const myColor = d3
       .scaleLinear()
@@ -86,7 +119,7 @@ export default class Heatmap extends Stanza {
       ])
       .domain([1, 100]);
 
-    svg
+    graphArea
       .selectAll()
       .data(dataset, function (d) {
         return d.group + ":" + d.variable;
@@ -117,11 +150,11 @@ export default class Heatmap extends Stanza {
   intervals(
     values,
     color,
-    steps = this.params["legend-groups"] > 2 ? this.params["legend-groups"] : 2
+    steps = this.params["legend-groups"] >= 2 ? this.params["legend-groups"] : 2
   ) {
     const [min, max] = [Math.min(...values), Math.max(...values)];
-    return [...Array(steps + 1).keys()].map((i) => {
-      const n = Math.round(min + i * ((max - min) / steps));
+    return [...Array(steps).keys()].map((i) => {
+      const n = Math.round(min + (i) * ((max - min) / steps));
       return {
         label: n,
         color: color(n),
