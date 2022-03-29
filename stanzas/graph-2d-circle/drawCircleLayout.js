579856371462a7ae68df6c6cd13d6b8eab752844
Anton Zhuravlev <nokker@gmail.com>
2022-03-29 14:47:27 +0900

    fix nodes color whet datakey val is 0

--- stanzas/graph-2d-circle/drawCircleLayout.js
+++ stanzas/graph-2d-circle/drawCircleLayout.js
@@ -6,7 +6,15 @@ export default function (svg, nodes, edges, params) {
   const edgesC = JSON.parse(JSON.stringify(edges));
 
   prepareGraphData(nodesC, edgesC, params);
-  const { width, height, MARGIN, symbols, labelsParams } = params;
+  const {
+    width,
+    height,
+    MARGIN,
+    symbols,
+    labelsParams,
+    tooltipParams,
+    highlightAdjEdges,
+  } = params;
 
   const HEIGHT = height - MARGIN.TOP - MARGIN.BOTTOM;
   const WIDTH = width - MARGIN.LEFT - MARGIN.RIGHT;
@@ -56,14 +64,17 @@ export default function (svg, nodes, edges, params) {
         `translate(${WIDTH / 2 + R},${HEIGHT / 2}) rotate(${angleScale(d.id)} ${
           -WIDTH / 2
         } 0)`
-    )
-    .attr("data-tooltip", (d) => d.id);
+    );
 
-  nodeGroups
+  const nodeCircles = nodeGroups
     .append("circle")
     .attr("class", "node")
     .style("fill", (d) => d[symbols.nodeColorSym])
     .attr("r", (d) => d[symbols.nodeSizeSym]);
+
+  if (tooltipParams.show) {
+    nodeCircles.attr("data-tooltip", (d) => d[tooltipParams.dataKey]);
+  }
 
   if (labelsParams.dataKey !== "" && nodesC[0][labelsParams.dataKey]) {
     nodeGroups
@@ -91,37 +102,39 @@ export default function (svg, nodes, edges, params) {
       .attr("class", "label");
   }
 
-  nodeGroups.on("mouseover", function (e, d) {
-    // highlight current node
-    d3.select(this).classed("active", true);
-    // fade out all other nodes, highlight a little connected ones
-    nodeGroups
-      .classed("fadeout", (p) => d !== p)
-      .classed("half-active", (p) => {
-        return (
-          p !== d &&
-          d[symbols.edgeSym].some(
-            (edge) =>
-              edge[symbols.sourceNodeSym] === p ||
-              edge[symbols.targetNodeSym] === p
-          )
-        );
-      });
+  if (highlightAdjEdges) {
+    nodeGroups.on("mouseover", function (e, d) {
+      // highlight current node
+      d3.select(this).classed("active", true);
+      // fade out all other nodes, highlight a little connected ones
+      nodeGroups
+        .classed("fadeout", (p) => d !== p)
+        .classed("half-active", (p) => {
+          return (
+            p !== d &&
+            d[symbols.edgeSym].some(
+              (edge) =>
+                edge[symbols.sourceNodeSym] === p ||
+                edge[symbols.targetNodeSym] === p
+            )
+          );
+        });
 
-    // fadeout not connected edges, highlight connected ones
-    links
-      .classed("fadeout", (p) => !d[symbols.edgeSym].includes(p))
-      .classed("active", (p) => d[symbols.edgeSym].includes(p));
-  });
+      // fadeout not connected edges, highlight connected ones
+      links
+        .classed("fadeout", (p) => !d[symbols.edgeSym].includes(p))
+        .classed("active", (p) => d[symbols.edgeSym].includes(p));
+    });
 
-  nodeGroups.on("mouseleave", function () {
-    links
-      .classed("active", false)
-      .classed("fadeout", false)
-      .classed("half-active", false);
-    nodeGroups
-      .classed("active", false)
-      .classed("fadeout", false)
-      .classed("half-active", false);
-  });
+    nodeGroups.on("mouseleave", function () {
+      links
+        .classed("active", false)
+        .classed("fadeout", false)
+        .classed("half-active", false);
+      nodeGroups
+        .classed("active", false)
+        .classed("fadeout", false)
+        .classed("half-active", false);
+    });
+  }
 }
