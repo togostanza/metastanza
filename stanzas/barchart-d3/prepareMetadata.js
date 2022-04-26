5f100d7b4c3b786f071cc36b1ada8a8bc86632d9
Anton <nokker@gmail.com>
2022-04-26 17:21:03 +0900

    add custom example page values, add common jsons

---
+++ stanzas/barchart-d3/prepareMetadata.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+const fs = require("fs");
+const merge = require("deepmerge-json");
+const { mergeExamples } = require("../../lib/replaceExampleMetadata");
+
+try {
+  const common = require("../../lib/json/stanzaCommon.json");
+  const axisXCommon = require("../../lib/json/axisXCommon.json");
+  const axisYCommon = require("../../lib/json/axisYCommon.json");
+  const DataSeriesCommon = require("../../lib/json/dataSeriesCommon.json");
+  const colorDatapointsCommon = require("../../lib/json/coloringDatapointsCommon.json");
+  const tooltipsCommon = require("../../lib/json/tooltipsCommon.json");
+  const thisStanzaParams = require("./thisStanzaParams.json");
+  const exampleValues = require("./customExampleValues.json");
+
+  const merged = merge.multi(
+    common,
+    thisStanzaParams,
+    axisXCommon,
+    axisYCommon,
+    DataSeriesCommon,
+    colorDatapointsCommon,
+    tooltipsCommon
+  );
+
+  const mergedWithExamplesParams = mergeExamples(
+    [merged["stanza:parameter"], exampleValues["stanza:parameter"]],
+    "stanza:key"
+  );
+  const mergedWithExamplesStyles = mergeExamples(
+    [merged["stanza:style"], exampleValues["stanza:style"]],
+    "stanza:key"
+  );
+
+  merged["stanza:parameter"] = mergedWithExamplesParams;
+  merged["stanza:style"] = mergedWithExamplesStyles;
+
+  const mergedJSON = JSON.stringify(merged, null, 2);
+  fs.writeFileSync("metadata.json", mergedJSON);
+  console.log("Done merging");
+} catch (error) {
+  console.error(error);
+}
