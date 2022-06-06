637355b5f8c99d1bbb57065337924120d076a84b
永野 朗夫 <nagano@me.com>
2022-06-06 21:27:07 +0900

    add lib

--- stanzas/test-color-generator/index.js
+++ stanzas/test-color-generator/index.js
@@ -1,6 +1,7 @@
 import Stanza from "togostanza/stanza";
 import loadData from "togostanza-utils/load-data";
 import Color from "color";
+import { ColorGenerator, getColorSeries } from "@/lib/ColorGenerator";
 import * as d3 from "d3";
 import {
   downloadSvgMenuItem,
@@ -33,7 +34,8 @@ export default class TestColorGenerator extends Stanza {
 
   async render() {
     appendCustomCss(this, this.params["custom-css-url"]);
-    this.colorSeries = this.getColorSeries();
+    this.colorSeries = getColorSeries(this);
+    console.log(this.colorSeries)
 
     this.renderTemplate({ template: "stanza.html.hbs" });
 
@@ -89,72 +91,6 @@ export default class TestColorGenerator extends Stanza {
     }).join('');
   }
 
-  // drawVennDiagram() {
-  //   return;
-  //   // set common parameters and styles
-  //   const container = this.root.querySelector("#venn-diagrams");
-  //   const svgWidth = this.params["width"];
-  //   const svgHeight = this.params["height"];
-  //   container.style.width = svgWidth + "px";
-  //   container.style.height = svgHeight + "px";
-
-  //   // show venn diagram corresponds to data(circle numbers to draw)
-  //   const selectedDiagram = this.root.querySelector(
-  //     `.venn-diagram[data-number-of-data="${this.numberOfData}"]`
-  //   );
-  //   if (!selectedDiagram) {
-  //     console.error(
-  //       "Venn diagrams with more than six elements are not supported. Please try using Euler diagrams."
-  //     );
-  //     return;
-  //   }
-  //   selectedDiagram.classList.add("-current");
-  //   this.venn.set("node", selectedDiagram);
-
-  //   // set scale
-  //   const containerRect = this.root
-  //     .querySelector("main")
-  //     .getBoundingClientRect();
-  //   const rect = selectedDiagram.getBoundingClientRect();
-  //   const margin = Math.max(rect.x - containerRect.x, rect.y - containerRect.y);
-  //   const scale = Math.min(
-  //     svgWidth / (rect.width + margin * 2),
-  //     svgHeight / (rect.height + margin * 2)
-  //   );
-  //   selectedDiagram.setAttribute("transform", `scale(${scale})`);
-  //   const labelFontSize = +window
-  //     .getComputedStyle(this.element)
-  //     .getPropertyValue("--togostanza-label-font-size")
-  //     .trim();
-  //   selectedDiagram.querySelectorAll("text").forEach((text) => {
-  //     text.style.fontSize = labelFontSize / scale + "px";
-  //   });
-
-  //   // shapes
-  //   selectedDiagram.querySelectorAll(":scope > g").forEach((group) => {
-  //     const targets = group.dataset.targets.split(",").map((target) => +target);
-  //     const labels = targets.map((target) => this.dataLabels[target]);
-  //     const count =
-  //       this.data.find((datum) => {
-  //         return (
-  //           datum.set.length === labels.length &&
-  //           labels.every((label) =>
-  //             datum.set.find((label2) => label === label2)
-  //           )
-  //         );
-  //       })?.size ?? "";
-  //     // set color
-  //     const color = this.getBlendedColor(targets);
-  //     group
-  //       .querySelector(":scope > .part")
-  //       .setAttribute("fill", color.toString());
-  //     // set label
-  //     group.querySelector(":scope > text.label").textContent = labels.join(",");
-  //     group.querySelector(":scope > text.value").textContent = count;
-  //   });
-
-  // }
-
   getColorSeries() {
     const getPropertyValue = (key) =>
       window.getComputedStyle(this.element).getPropertyValue(key);
@@ -165,41 +101,5 @@ export default class TestColorGenerator extends Stanza {
     return series.map((variable) => getPropertyValue(variable).trim());
   }
 
-  // getBlendedColor(targets) {
-  //   let blendedColor = Color(this.colorSeries[targets[0]]);
-  //   targets.forEach((target, index) => {
-  //     if (index > 0) {
-  //       blendedColor = blendedColor.mix(
-  //         Color(this.colorSeries[target]),
-  //         1 / (index + 1)
-  //       );
-  //     }
-  //   });
-  //   const ratio = (targets.length - 1) / (this.numberOfData - 1);
-  //   switch (this.params["blend-mode"]) {
-  //     case "multiply":
-  //       blendedColor = blendedColor.saturate(ratio);
-  //       blendedColor = blendedColor.darken(ratio * 0.5);
-  //       break;
-  //     case "screen":
-  //       blendedColor = blendedColor.saturate(ratio);
-  //       blendedColor = blendedColor.lighten(ratio * 0.5);
-  //       break;
-  //   }
-  //   return blendedColor;
-  // }
 
-  // async getData() {
-  //   const data = await loadData(
-  //     this.params["data-url"],
-  //     this.params["data-type"],
-  //     this.root.querySelector("main")
-  //   );
-  //   // // processing
-  //   // for (const datum of data) {
-  //   //   datum.orgs = datum.orgs.split(', ');
-  //   //   datum.count = Number(datum.count);
-  //   // }
-  //   return data;
-  // }
 }
