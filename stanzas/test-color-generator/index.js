86c37e141200c4d7c9b0515b0b2e4b7648f3d3e5
行事光 <h.gyoji.vrds@gmail.com>
2022-06-17 18:54:45 +0900

    fix npm run lint:fix

--- stanzas/test-color-generator/index.js
+++ stanzas/test-color-generator/index.js
@@ -1,5 +1,10 @@
 import Stanza from "togostanza/stanza";
-import { InterpolateColorGenerator, CirculateColorGenerator, getColorSeries, getColorsWithD3PresetColor } from "@/lib/ColorGenerator";
+import {
+  InterpolateColorGenerator,
+  CirculateColorGenerator,
+  getColorSeries,
+  getColorsWithD3PresetColor,
+} from "@/lib/ColorGenerator";
 import {
   downloadSvgMenuItem,
   downloadPngMenuItem,
@@ -10,7 +15,6 @@ import {
 } from "togostanza-utils";
 
 export default class TestColorGenerator extends Stanza {
-
   menu() {
     return [
       downloadSvgMenuItem(this, "TestColorGenerator"),
@@ -26,82 +30,114 @@ export default class TestColorGenerator extends Stanza {
 
     this.renderTemplate({ template: "stanza.html.hbs" });
 
-    const colorChips = this.root.querySelector('#color-chips');
+    const colorChips = this.root.querySelector("#color-chips");
 
     // get parameters
-    const numOfGradation = this.params['number-of-gradation'];
-    const colorCirculation = this.params['type-of-color-circulation'];
-    const colorDomain = this.params['color-domain']? this.params['color-domain'].split(',').map(pos => +pos) : "";
-    const colorRange = this.params['color-range'].split(',');
-    const NumOfMakeColor = this.params['number-of-make-color'];
+    const numOfGradation = this.params["number-of-gradation"];
+    const colorCirculation = this.params["type-of-color-circulation"];
+    const colorDomain = this.params["color-domain"]
+      ? this.params["color-domain"].split(",").map((pos) => +pos)
+      : "";
+    const colorRange = this.params["color-range"].split(",");
+    const NumOfMakeColor = this.params["number-of-make-color"];
+    let d3ColorScheme = this.params["color-scheme"];
+    let colors = [],
+      colorGenerator;
 
-    let d3ColorScheme = this.params['color-scheme'];
-
-    let colors = [], colorGenerator;
-    
     if (d3ColorScheme.indexOf("(") !== -1) {
       d3ColorScheme = d3ColorScheme.substr(0, d3ColorScheme.indexOf(" ("));
     }
 
-    if (colorCirculation === 'interpolate') {
-      if (colorDomain.length !== 1 && colorRange.length !== 1 && colorDomain.length === colorRange.length) {
+    if (colorCirculation === "interpolate") {
+      if (
+        colorDomain.length !== 1 &&
+        colorRange.length !== 1 &&
+        colorDomain.length === colorRange.length
+      ) {
         colorGenerator = new InterpolateColorGenerator(colorRange, colorDomain);
-      } else if (d3ColorScheme.indexOf("Categorical-") !== -1){
+      } else if (d3ColorScheme.indexOf("Categorical-") !== -1) {
         // categorical
         d3ColorScheme = d3ColorScheme.replace("Categorical-", "");
-        if (d3ColorScheme === 'Custom') {
+        if (d3ColorScheme === "Custom") {
           colors = getColorSeries(this);
-          colorGenerator = new InterpolateColorGenerator(colors,undefined);
+          colorGenerator = new InterpolateColorGenerator(colors, undefined);
         } else {
-          const colors = getColorsWithD3PresetColor(`scheme${d3ColorScheme}`, numOfGradation);
-          colorGenerator = new InterpolateColorGenerator(colors,undefined);
+          const colors = getColorsWithD3PresetColor(
+            `scheme${d3ColorScheme}`,
+            numOfGradation
+          );
+          colorGenerator = new InterpolateColorGenerator(colors, undefined);
         }
       } else {
         // continuous
         d3ColorScheme = d3ColorScheme.replace("Continuous-", "");
-        const colors = getColorsWithD3PresetColor(`interpolate${d3ColorScheme}`, numOfGradation);
-        colorGenerator = new InterpolateColorGenerator(colors,undefined);
+        const colors = getColorsWithD3PresetColor(
+          `interpolate${d3ColorScheme}`,
+          numOfGradation
+        );
+        colorGenerator = new InterpolateColorGenerator(colors, undefined);
       }
     } else {
-      if (colorDomain.length !== 1 && colorRange.length !== 1 && colorDomain.length === colorRange.length) {
+      if (
+        colorDomain.length !== 1 &&
+        colorRange.length !== 1 &&
+        colorDomain.length === colorRange.length
+      ) {
         colorGenerator = new CirculateColorGenerator(
-          colorRange, colorDomain, NumOfMakeColor);
+          colorRange,
+          colorDomain,
+          NumOfMakeColor
+        );
       } else if (colorRange[0].length) {
         colorGenerator = new CirculateColorGenerator(colorRange, undefined);
-      } else if (d3ColorScheme.indexOf("Categorical-") !== -1){
+      } else if (d3ColorScheme.indexOf("Categorical-") !== -1) {
         // categorical
         d3ColorScheme = d3ColorScheme.replace("Categorical-", "");
-        if (d3ColorScheme === 'Custom') {
+        if (d3ColorScheme === "Custom") {
           colors = getColorSeries(this);
-          colorGenerator = new CirculateColorGenerator(colors,undefined);
+          colorGenerator = new CirculateColorGenerator(colors, undefined);
         } else {
-          const colors = getColorsWithD3PresetColor(`scheme${d3ColorScheme}`, numOfGradation);
-          colorGenerator = new CirculateColorGenerator(colors,undefined);
+          const colors = getColorsWithD3PresetColor(
+            `scheme${d3ColorScheme}`,
+            numOfGradation
+          );
+          colorGenerator = new CirculateColorGenerator(colors, undefined);
         }
       } else {
         // continuous
         d3ColorScheme = d3ColorScheme.replace("Continuous-", "");
-        const colors = getColorsWithD3PresetColor(`interpolate${d3ColorScheme}`, numOfGradation);
-        colorGenerator = new CirculateColorGenerator(colors,undefined);
+        const colors = getColorsWithD3PresetColor(
+          `interpolate${d3ColorScheme}`,
+          numOfGradation
+        );
+        colorGenerator = new CirculateColorGenerator(colors, undefined);
       }
     }
 
     switch (colorCirculation) {
-      case 'circulate':
-        colorChips.innerHTML = [...Array(numOfGradation)].map((_, index) => {
-          return `<li data-pos="${index}" style="background: ${colorGenerator.get(index)}"></li>`
-        }).join('');
+      case "circulate":
+        colorChips.innerHTML = [...Array(numOfGradation)]
+          .map((_, index) => {
+            return `<li data-pos="${index}" style="background: ${colorGenerator.get(
+              index
+            )}"></li>`;
+          })
+          .join("");
         break;
 
-      case 'interpolate': {
-        const unit = 1 / (numOfGradation - 1);
-        colorChips.innerHTML = [...Array(numOfGradation)].map((_, index) => {
-          return `<li data-pos="${index * unit}" style="background: ${colorGenerator.get(index * unit)}"></li>`
-        }).join('');
-      }
+      case "interpolate":
+        {
+          const unit = 1 / (numOfGradation - 1);
+          colorChips.innerHTML = [...Array(numOfGradation)]
+            .map((_, index) => {
+              return `<li data-pos="${
+                index * unit
+              }" style="background: ${colorGenerator.get(index * unit)}"></li>`;
+            })
+            .join("");
+        }
         break;
     }
-
   }
 
   getColorSeries() {
@@ -113,5 +149,4 @@ export default class TestColorGenerator extends Stanza {
     }
     return series.map((variable) => getPropertyValue(variable).trim());
   }
-
 }
