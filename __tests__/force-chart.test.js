fc69e5601a016b95f7593591e858f6561969a0b2
Anton <nokker@gmail.com>
2022-10-25 11:00:16 +0900

    add test (to try), delete double param

--- __tests__/force-chart.test.js
+++ __tests__/force-chart.test.js
@@ -1,21 +1,32 @@
 import puppeteer from "puppeteer";
+import { error } from "console";
 
-test("force chart test", async () => {
-  let browser = await puppeteer.launch({
-    headless: true,
-  });
-
-  let page = await browser.newPage();
-
-  await page.goto("http://localhost:8080/force-graph.html");
-
-  await page.waitForSelector("togostanza-force-graph");
-
-  const dataType = await page.$eval("togostanza-force-graph", (e) =>
-    e.getAttribute("data-type")
-  );
-
-  expect(dataType).toBe("json");
-
-  browser.close();
+let browser = await puppeteer.launch({
+  headless: true,
 });
+let page = await browser.newPage();
+await page.goto("http://localhost:8080/force-graph.html");
+
+try {
+  //--silent=false
+  test("force chart test", async () => {
+    await page.waitForSelector("togostanza-force-graph");
+
+    await page.click("#edge-show_arrows");
+
+    const isArrowsHidden = await page.evaluate(() => {
+      error("hi!");
+      return document
+        .querySelector("togostanza-force-graph")
+        .shadowRoot.querySelector("#force-graph > svg > defs");
+    });
+
+    error(isArrowsHidden);
+
+    expect(1).toBe(1);
+  });
+} catch (e) {
+  error(e);
+} finally {
+  browser.close();
+}
